ef2a7ae6983c4cdcc10bcc6d1bb9313593e08f6f
Jasper Behrensdorf <jasper.behrensdorf@gmail.com>
2019-07-02 01:07:35 +0200

    Add demo script for cantilever beam example
    Everything is done "native" no cossan methods have been implemented yet.

---
+++ demo/cantilever.jl
@@ -0,0 +1,46 @@
+using Random, Distributions, LinearAlgebra, DataFrames
+
+nmc = 100000
+
+l = 1.8
+b = 0.12
+max_displacement = 0.01
+
+h = Normal(0.24, 0.01)
+P = LogNormal(log(5000^2/sqrt(400 + 5000^2)), sqrt(log(400/5000^2 + 1)))
+rho = LogNormal(log(600^2/sqrt(140 + 600^2)), sqrt(log(140/600^2 + 1)))
+E = LogNormal(log(10e9^2/sqrt(1.6e9 + 10e9^2)), sqrt(log(1.6e9/10e9^2 + 1)))
+
+corr = Matrix{Float64}(I, 4, 4)
+corr[3,2] = 0.8
+corr[2,3] = 0.8
+
+a = cholesky(corr).L
+z = rand(Normal(), nmc, 4)
+x = cdf.(Normal(), transpose(a * transpose(z)))
+
+samples = DataFrame(b = fill(b, nmc), l = fill(l, nmc))
+samples.h = quantile.(h, x[:, 1])
+samples.P = quantile.(P, x[:, 2])
+samples.rho = quantile.(rho, x[:, 3])
+samples.E = quantile.(E, x[:, 4])
+
+function inertia(df)
+    df.b .* df.h .^ 3 / 12
+end
+
+samples.I = inertia(samples)
+
+function displacement(df)
+    (df.rho .* 9.81 .* df.b .* df.h .* df.l .^ 4) ./ (8 .* df.E .* df.I) .+ (df.P .* df.l .^ 3) ./ (3 .* df.E .* df.I)
+end
+
+samples.w = displacement(samples)
+
+performance = -samples.w .+ max_displacement
+
+pf = sum(performance .< 0) / nmc
+
+println("Probability of failure: ", pf)
+
+
