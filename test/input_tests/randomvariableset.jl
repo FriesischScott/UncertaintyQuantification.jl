d8b1219ece31b97abfc24e05c5180c0af6ff9c12
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2019-08-01 13:45:35 +0200

    Add test for error check

--- test/input_tests/randomvariableset.jl
+++ test/input_tests/randomvariableset.jl
@@ -4,19 +4,20 @@ using UncertaintyQuantification
 using Distributions
 using Test
 
-rv1 = Normal(0,1);
-rv2 = Normal(0,1);
-
+rv1 = Normal(0,1)
+rv2 = Normal(0,1)
 
 rvs_ = [rv1 rv2]
-names_ = ["Rv1" "Rv2"];
-corr_ = [1 0.8; 0.8 1];
+names_ = ["Rv1" "Rv2"]
+corr_ = [1 0.8; 0.8 1]
 
-@testset "constructor tests" begin
+@testset "random variable sets" begin
 
-    @test typeof(RandomVariableSet(rvs_, names_)) == RandomVariableSet;
-    @test typeof(RandomVariableSet(rvs_, names_, corr_)) == RandomVariableSet;
-    @test typeof(RandomVariableSet(members = rvs_, names = names_)) == RandomVariableSet;
-    @test typeof(RandomVariableSet(members = rvs_, names = names_, corr = corr_)) == RandomVariableSet;
+    @test typeof(RandomVariableSet(rvs_, names_)) == RandomVariableSet
+    @test typeof(RandomVariableSet(rvs_, names_, corr_)) == RandomVariableSet
+    @test typeof(RandomVariableSet(members = rvs_, names = names_)) == RandomVariableSet
+    @test typeof(RandomVariableSet(members = rvs_, names = names_, corr = corr_)) == RandomVariableSet
+
+    @test_throws ErrorException("length(members) != length(names)") RandomVariableSet(rvs_, ["Rv1"])
 
 end
