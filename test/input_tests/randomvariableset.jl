e978dda604ce7cc3c74045281e17199017d85473
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2019-08-01 13:53:15 +0200

    Use RandomVariableSet as test set name
    Group using statements

--- test/input_tests/randomvariableset.jl
+++ test/input_tests/randomvariableset.jl
@@ -1,8 +1,5 @@
 
-using UncertaintyQuantification
-
-using Distributions
-using Test
+using Test, Distributions, UncertaintyQuantification
 
 rv1 = Normal(0,1)
 rv2 = Normal(0,1)
@@ -11,7 +8,7 @@ rvs_ = [rv1 rv2]
 names_ = ["Rv1" "Rv2"]
 corr_ = [1 0.8; 0.8 1]
 
-@testset "random variable sets" begin
+@testset "RandomVariableSet" begin
 
     @test typeof(RandomVariableSet(rvs_, names_)) == RandomVariableSet
     @test typeof(RandomVariableSet(rvs_, names_, corr_)) == RandomVariableSet
