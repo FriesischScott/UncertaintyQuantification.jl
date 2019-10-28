40c4b2ccb60d7d05f36dbae09c1031cb4f52fa2b
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2019-10-28 17:05:50 +0100

    Fix test for RandomVariableSet

--- test/inputs/randomvariableset.jl
+++ test/inputs/randomvariableset.jl
@@ -1,21 +1,25 @@
 
 using Test, Distributions, UncertaintyQuantification
 
-rv1 = Normal(0,1)
-rv2 = Normal(0,1)
+rv1 = RandomVariable(Normal(0, 1), "Rv1")
+rv2 = RandomVariable(Normal(0, 1), "RV2")
 
 rvs_ = [rv1 rv2]
-names_ = ["Rv1" "Rv2"]
 corr_ = [1 0.8; 0.8 1]
 
 @testset "RandomVariableSet" begin
 
-    @test typeof(RandomVariableSet(rvs_, names_)) == RandomVariableSet
-    @test typeof(RandomVariableSet(rvs_, names_, corr_)) == RandomVariableSet
-    @test typeof(RandomVariableSet(members = rvs_, names = names_)) == RandomVariableSet
-    @test typeof(RandomVariableSet(members = rvs_, names = names_, corr = corr_)) == RandomVariableSet
+    @test typeof(RandomVariableSet(rvs_)) == RandomVariableSet
+    @test typeof(RandomVariableSet(rvs_, corr_)) == RandomVariableSet
+    @test typeof(RandomVariableSet(members = rvs_)) == RandomVariableSet
+    @test typeof(RandomVariableSet(
+        members = rvs_,
+        corr = corr_,
+    )) == RandomVariableSet
 
-    @test_throws ErrorException("length(members) != length(names)") RandomVariableSet(rvs_, ["Rv1"])
-    @test_throws ErrorException("wrong dimension of correlation matrix") RandomVariableSet(rvs_, names_, [1 0 0; 0 1 0; 0 0 1])
+    @test_throws ErrorException("wrong dimension of correlation matrix") RandomVariableSet(
+        rvs_,
+        [1 0 0; 0 1 0; 0 0 1],
+    )
 
 end
