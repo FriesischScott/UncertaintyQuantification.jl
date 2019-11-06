ebcf334c4182d4dbc62b212b354586ee68617398
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2019-11-06 14:54:28 +0100

    Test RandomVariableSet for values and types

--- test/inputs/randomvariableset.jl
+++ test/inputs/randomvariableset.jl
@@ -1,6 +1,3 @@
-
-using Test, Distributions, UncertaintyQuantification
-
 rv1 = RandomVariable(Normal(0, 1), "Rv1")
 rv2 = RandomVariable(Normal(0, 1), "RV2")
 
@@ -16,6 +13,15 @@ corr_ = [1 0.8; 0.8 1]
         members = rvs_,
         corr = corr_,
     )) == RandomVariableSet
+
+    rvset = RandomVariableSet(rvs_, corr_)
+
+    @test rvset.members == rvs_
+    @test rvset.corr == corr_
+
+    rvset = RandomVariableSet(members = rvs_)
+
+    @test rvset.corr == [1 0; 0 1]
 
     @test_throws ErrorException("wrong dimension of correlation matrix") RandomVariableSet(
         rvs_,
