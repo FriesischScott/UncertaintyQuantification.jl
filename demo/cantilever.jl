12ae98f8ee339b3c6084b511a1aeb56ba65c1f22
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2019-11-14 16:16:26 +0100

    Fix standard deviations and correlation
    Turns out the results are correct if you stop correlating the load and the density instead of Young's modulus and density.

--- demo/cantilever.jl
+++ demo/cantilever.jl
@@ -6,22 +6,22 @@ b = Parameter(0.12, "b")
 h = RandomVariable(Normal(0.24, 0.01), "h")
 E = RandomVariable(
     LogNormal(
-        log(10e9^2 / sqrt(1.6e9 + 10e9^2)),
-        sqrt(log(1.6e9 / 10e9^2 + 1)),
+        log(10e9^2 / sqrt(1.6e9^2 + 10e9^2)),
+        sqrt(log(1.6e9^2 / 10e9^2 + 1)),
     ),
     "E",
 )
 
 P = RandomVariable(
-    LogNormal(log(5000^2 / sqrt(400 + 5000^2)), sqrt(log(400 / 5000^2 + 1))),
+    LogNormal(log(5000^2 / sqrt(400^2 + 5000^2)), sqrt(log(400^2 / 5000^2 + 1))),
     "P",
 )
 ρ = RandomVariable(
-    LogNormal(log(600^2 / sqrt(140 + 600^2)), sqrt(log(140 / 600^2 + 1))),
+    LogNormal(log(600^2 / sqrt(140^2 + 600^2)), sqrt(log(140^2 / 600^2 + 1))),
     "ρ",
 )
 
-rvset = RandomVariableSet([P ρ], [1 0.8; 0.8 1])
+rvset = RandomVariableSet([E ρ], [1 0.8; 0.8 1])
 
 inertia = Model(df -> df.b .* df.h .^ 3 / 12, "I")
 
@@ -37,7 +37,7 @@ max_displacement = 0.01
 pf, samples = probability_of_failure(
     [inertia, displacement],
     df -> max_displacement .- df.w,
-    [l, b, h, E, rvset],
+    [l, b, h, P, rvset],
     mc,
 )
 
