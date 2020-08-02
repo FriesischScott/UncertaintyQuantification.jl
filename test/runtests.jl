0de144c122b7b79ad20941bac5c5b239be72b13e
sitoryu <60561349+sitoryu@users.noreply.github.com>
2020-08-02 20:35:56 +0200

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,9 +8,11 @@ include("inputs/inputs.jl")
 include("inputs/copulas/gaussian.jl")
 
 include("models/model.jl")
+include("models/polyspline.jl")
 
 include("reliability/probabilityoffailure.jl")
 
 include("sensitivity/gradient.jl")
+include("sensitivity/soindex.jl")
 
 include("simulations/montecarlo.jl")
