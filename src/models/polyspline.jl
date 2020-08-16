a0a096cd612cd474f6524535f20233e0351657a3
sitoryu <60561349+sitoryu@users.noreply.github.com>
2020-08-16 23:55:13 +0200

    -> outer constructor to inner constructor
    PolySpline to Polyharmonic Spline
    createpolyspline is now changed to inner constructor Polyharmonic spline
    changed name phi to greek alphabet

--- src/models/polyspline.jl
+++ src/models/polyspline.jl
@@ -1,18 +1,50 @@
-struct PolySpline <: UQModel
+struct PolyharmonicSpline <: UQModel
     w::Array{Float64}
     v::Array{Float64}
     ci::Array{Float64}
     k::Int64
-    orderfunc::Function
+    orderfunc
     name::Symbol
+
+    function PolyharmonicSpline(
+        inputs::Array{},
+        f::Array{},
+        k::Int64,
+        name::Symbol,
+        orderfunc = 0 #grabs the needed inputs from a Dataframe and returns a vector of said inputs
+        #used to pull out the correct variables from the dataframe in case of multiple models
+
+        )
+        inputN = size(f, 1)
+
+        A = Array{Float64}(undef, inputN, inputN)
+        for i in 1:inputN
+            for j in 1:inputN
+                r = sqrt(dot((inputs[i, :] .- inputs[j, :]), (inputs[i, :] .- inputs[j, :])))
+                A[i,j] = ϕ(r, k)
+            end
+        end
+        B = [ones(inputN, 1) inputs]
+        Bt = transpose(B)
+
+
+        Mlin = [A B; Bt zeros(size(B, 2),size(B, 2))]
+
+        Flin = [f; zeros(size(B, 2), 1)]
+
+        wv = Mlin \ Flin
+
+        w = wv[1:inputN,:]
+        v = wv[inputN+1:size(wv, 1),:]
+
+        new(w, v, inputs, k, orderfunc, name)
+    end
 end
 
-function phi(
-    x::Vector{},
-    c::Vector{},
+function ϕ(
+    r::Float64,
     k::Int64
     )
-    r = sqrt(dot((x .- c), (x .- c)))
     if k%2 != 0
         return r^k
     elseif r < 1
@@ -22,46 +54,15 @@ function phi(
     end
 end
 
-function createpolyspline(
-    inputs::Array{},
-    f::Array{},
-    k::Int64,
-    name::Symbol,
-    orderfunc::Function = (df -> []) # needed to pull out the correct variables from the dataframe in case of using soindex or multiple models
-)
-    inputN = size(f, 1)
-
-    A = Array{Float64}(undef, inputN, inputN)
-    for i in 1:inputN
-        for j in 1:inputN
-            A[i,j] = phi(inputs[i, :],inputs[j, :],k)
-        end
-    end
-    B = [ones(inputN, 1) inputs]
-    Bt = transpose(B)
-
-
-    Mlin = [A B; Bt zeros(size(B, 2),size(B, 2))]
-
-    Flin = [f; zeros(size(B, 2), 1)]
-
-    wv = Mlin \ Flin
-
-    w = wv[1:inputN,:]
-    v = wv[inputN+1:size(wv, 1),:]
-
-    polymodel = PolySpline(w, v, inputs, k, orderfunc,name)
-    return polymodel
-end
-
 function calcpolyspline(
-    po::PolySpline,
+    po::PolyharmonicSpline,
     x::Array{}
     )
     rv = 0
 
     for i in 1:size(po.ci, 1)
-        fp = po.w[i,1] * phi(x, po.ci[i, :], po.k) # adding radial basis function
+        r = sqrt(dot((x .- po.ci[i, :]), (x .- po.ci[i, :])))
+        fp = po.w[i,1] * ϕ(r, po.k) # adding radial basis function
         rv += fp[1,1]
     end
     xv = [ones(1,1); x]
@@ -70,16 +71,20 @@ function calcpolyspline(
 end
 
 function evaluate!(
-    po::PolySpline,
+    po::PolyharmonicSpline,
     df::DataFrame
     )
     # this function is designed to act like evaluate! in model.jl
     # it takes a model and dataframe and creates a new column: model.name
     # the model is appplied to each row of the dataframe and stashes the solution in the new column
     # use is only possible if the PolySpline has a real orderfunction
-    psinput = po.orderfunc(df)
-    out = Vector{Float64}(undef, size(psinput, 1))
+    if po.orderfunc == 0 # only works with a single Polyharmonic Spline as model
+        psinput = convert(Matrix, df[:,:])
+    else    # multiple models
+        psinput = po.orderfunc(df)
+    end
 
+    out = Vector{Float64}(undef, size(psinput, 1))
     for i in 1:size(psinput, 1)
         out[i] = calcpolyspline(po, psinput[i, :])
     end
