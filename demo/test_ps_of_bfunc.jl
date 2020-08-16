36af7fb75539928ceb81f7cfabe3e5c0d4002328
sitoryu <60561349+sitoryu@users.noreply.github.com>
2020-08-16 23:51:47 +0200

    new input/constructor

--- demo/test_ps_of_bfunc.jl
+++ demo/test_ps_of_bfunc.jl
@@ -1,31 +1,32 @@
-using UncertaintyQuantification
+using UncertaintyQuantification, Plots
 
 # Die normale B-Funktion
 
-inputs = Vector{RandomVariable}(undef, 20)
+inputs = Vector{RandomVariable}(undef, 10)
 
 X = [1,1.1,0.9,1.2,0.8,0.7,1.3,1.4,0.6,0.95]
 
-for i in 1:10
-    inputs[i] = RandomVariable(Normal(0, X[i]), Symbol("A$i"))
-    inputs[i+10] = RandomVariable(Normal(0, X[i]), Symbol("B$i"))
+for i in 1:5
+    inputs[i] = RandomVariable(Normal(0, X[i]), Symbol("x$i"))
+    inputs[i+5] = RandomVariable(Normal(0, X[i+5]), Symbol("w$i"))
 end
 
 
-bfkt = Model(df -> df.A1 .* df.A6 + df.A2 .* df.A7 + df.A3 .* df.A8
-+ df.A4 .* df.A9 + df.A5 .* df.A10, :fkt)
+bfkt = Model(df -> df.x1 .* df.w1 + df.x2 .* df.w2 + df.x3 .* df.w3
++ df.x4 .* df.w4 + df.x5 .* df.w5, :fkt)
 
 
-mc = MonteCarlo(1000)
+mc = MonteCarlo(10000)
 
-si = soindex([bfkt], inputs, mc)
+si = sobolindices([bfkt], inputs, mc)
 
 #Polyspline der B-Funktion
 
 inputsb = Vector{RandomVariable}(undef, 10)
 
-for i in 1:10
-    inputsb[i] = RandomVariable(Normal(0, X[i]), Symbol("A$i"))
+for i in 1:5
+    inputsb[i] = RandomVariable(Normal(0, X[i]), Symbol("x$i"))
+    inputsb[i+5] = RandomVariable(Normal(0, X[i+5]), Symbol("w$i"))
 end
 
 ps_samples = sample(inputsb, 1000)
@@ -36,8 +37,8 @@ ps_samples_f = convert(Matrix, ps_samples[:,11:11])
 
 k = 2
 
-ps_order = df -> [df.A1 df.A2 df.A3 df.A4 df.A5 df.A6 df.A7 df.A8 df.A9 df.A10]
+ps_order = df -> [df.x1 df.x2 df.x3 df.x4 df.x5 df.w1 df.w2 df.w3 df.w4 df.w5]
 
-ps_b = createpolyspline(ps_samples_ci, ps_samples_f, k, :fkt, ps_order)
+ps_b = PolyharmonicSpline(ps_samples_ci, ps_samples_f, k, :fkt)
 
-si_ps = soindex([ps_b], inputs, mc)
+si_ps_k_2 = sobolindices([ps_b], inputs, mc)
