40c1718214edea382c3afea8ac2825888508701e
sitoryu <60561349+sitoryu@users.noreply.github.com>
2020-08-16 23:59:34 +0200

    Update and rename polyspline.jl to polyharmonicspline.jl

--- test/models/polyharmonicspline.jl
+++ test/models/polyharmonicspline.jl
@@ -1,4 +1,4 @@
-@testset "PolySpline" begin
+@testset "PolyharmonicSpline" begin
     A = [1 2; 1 3; 3 6; 4 3; 5 7; 5 2; 6 8; 6 9; 3 7]
     f = [2; 3; 18; 12; 35; 10; 48; 54; 21]
     k = 1
@@ -6,18 +6,18 @@
 
     input = DataFrame(a = 4, b = 3)
 
-    polyspline = createpolyspline(A, f, k, :fkt, order)
+    polyspline = PolyharmonicSpline(A, f, k, :fkt, order)
 
     B = Array{Float64}(undef, 1)
     B[1] = 12
 
-    @test isa(polyspline, PolySpline)
+    @test isa(polyspline, PolyharmonicSpline)
 
     B = Array{Float64}(undef, 1)
     B[1] = 12
 
     evaluate!(polyspline, input)
-    
+
     A = input.fkt
     A = round.(A, digits=3)
 
