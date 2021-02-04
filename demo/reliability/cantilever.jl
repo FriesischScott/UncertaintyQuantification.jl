be2319669119a238e6447610363a2dc4233cbd73
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2021-02-04 14:21:01 +0100

    Refactor cantilever example slightly

--- demo/reliability/cantilever.jl
+++ demo/reliability/cantilever.jl
@@ -1,36 +1,32 @@
 using UncertaintyQuantification
 
-l = Parameter(1.8, :l)
-b = Parameter(0.12, :b)
+l = Parameter(1.8, :l) # length
+b = Parameter(0.12, :b) # width
 
-h = RandomVariable(Normal(0.24, 0.01), :h)
-E = RandomVariable(
-    LogNormal(
-        log(10e9^2 / sqrt(1.6e9^2 + 10e9^2)),
-        sqrt(log(1.6e9^2 / 10e9^2 + 1)),
-    ),
-    :E,
-)
+h = RandomVariable(Normal(0.24, 0.01), :h) # height
 
-P = RandomVariable(
-    LogNormal(log(5000^2 / sqrt(400^2 + 5000^2)), sqrt(log(400^2 / 5000^2 + 1))),
-    :P,
-)
-ρ = RandomVariable(
-    LogNormal(log(600^2 / sqrt(140^2 + 600^2)), sqrt(log(140^2 / 600^2 + 1))),
-    :ρ,
-)
+μ = log(10e9^2 / sqrt(1.6e9^2 + 10e9^2))
+σ = sqrt(log(1.6e9^2 / 10e9^2 + 1))
+E = RandomVariable(LogNormal(μ, σ), :E) # young's modulus
+
+μ = log(5000^2 / sqrt(400^2 + 5000^2))
+σ = sqrt(log(400^2 / 5000^2 + 1))
+P = RandomVariable(LogNormal(μ, σ), :P) # tip load
+
+μ = log(600^2 / sqrt(140^2 + 600^2))
+σ = sqrt(log(140^2 / 600^2 + 1))
+ρ = RandomVariable(LogNormal(μ, σ), :ρ) # density
 
 c = GaussianCopula([1 0.8; 0.8 1])
 jd = JointDistribution([E ρ], c)
 
 inputs = [l, b, h, P, jd]
 
-inertia = Model(df -> df.b .* df.h .^ 3 / 12, :I)
+inertia = Model(df -> df.b .* df.h.^3 / 12, :I)
 
 displacement = Model(
-    df -> (df.ρ .* 9.81 .* df.b .* df.h .* df.l .^ 4) ./
-          (8 .* df.E .* df.I) .+ (df.P .* df.l .^ 3) ./ (3 .* df.E .* df.I),
+    df -> (df.ρ .* 9.81 .* df.b .* df.h .* df.l.^4) ./
+          (8 .* df.E .* df.I) .+ (df.P .* df.l.^3) ./ (3 .* df.E .* df.I),
     :w,
 )
 
