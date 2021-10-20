39c993bb77a3bc456c304f49edfe59774d34bee1
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2021-10-20 18:03:06 +0200

    Add tests for first order indices

--- test/sensitivity/sobolindices.jl
+++ test/sensitivity/sobolindices.jl
@@ -1,27 +1,51 @@
 @testset "sobolindices" begin
 
-    σx = [1, 1.1, 0.9, 1.2, 0.8]
-    σω = [0.7, 1.3, 1.4, 0.6, 0.95]
+    x = RandomVariable.(Uniform(-π, π), [:x1, :x2, :x3])
 
-    X = RandomVariable.(Normal.(0, σx), [:X1, :X2, :X3, :X4, :X5])
-    ω = RandomVariable.(Normal.(0, σω), [:ω1, :ω2, :ω3, :ω4, :ω5])
+    ishigami = Model(
+        df -> sin.(df.x1) + 7 .* sin.(df.x2) .^ 2 + 0.1 .* df.x3 .^ 4 .* sin.(df.x1),
+        :f,
+    )
 
-    B = Model(df -> df.X1 .* df.ω1
-    + df.X2 .* df.ω2
-    + df.X3 .* df.ω3
-    + df.X4 .* df.ω4
-    + df.X5 .* df.ω5, :B)
+    n = 1000
 
-    Random.seed!(8128)
+    @testset "Monte Carlo" begin
+        Random.seed!(8128)
 
-    si = sobolindices([B], [X; ω], :B, MonteCarlo(5000))
+        si = sobolindices([ishigami], x, :f, MonteCarlo(n))
 
-    Random.seed!()
+        Random.seed!()
 
-    VB = sum(σx.^2 .* σω.^2)
-    analytical = (σx.^2 .* σω.^2) / VB
+        @test isapprox(si.FirstOrder, [0.314, 0.442, 0.00], rtol = 0.1) |> all
+    end
 
-    @test isapprox.(si.FirstOrder, 0.0, atol=0.1) |> all
-    @test isapprox.(si.TotalEffect[1:5], analytical, rtol=0.1) |> all
-    @test isapprox.(si.TotalEffect[6:end], analytical, rtol=0.1) |> all
+    @testset "Sobol" begin
+        Random.seed!(8128)
+
+        si = sobolindices([ishigami], x, :f, SobolSampling(n))
+
+        Random.seed!()
+
+        @test isapprox(si.FirstOrder, [0.314, 0.442, 0.00], rtol = 0.1) |> all
+    end
+
+    @testset "Halton" begin
+        Random.seed!(8128)
+
+        si = sobolindices([ishigami], x, :f, MonteCarlo(n))
+
+        Random.seed!()
+
+        @test isapprox(si.FirstOrder, [0.314, 0.442, 0.00], rtol = 0.1) |> all
+    end
+
+    @testset "Latin Hypercube" begin
+        Random.seed!(8128)
+
+        si = sobolindices([ishigami], x, :f, MonteCarlo(n))
+
+        Random.seed!()
+
+        @test isapprox(si.FirstOrder, [0.314, 0.442, 0.00], rtol = 0.1) |> all
+    end
 end
