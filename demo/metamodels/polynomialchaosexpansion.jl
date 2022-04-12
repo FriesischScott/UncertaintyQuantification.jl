476377b889c2a3e72e0d554d24471c42b06bc275
Ander Gray <ander.gray@liverpool.ac.uk>
2022-04-12 14:18:01 +0100

    adds MC and histograms to pce example

--- demo/metamodels/polynomialchaosexpansion.jl
+++ demo/metamodels/polynomialchaosexpansion.jl
@@ -1,4 +1,4 @@
-using UncertaintyQuantification
+using UncertaintyQuantification, Plots
 
 x = RandomVariable.(Uniform(-1, 1), [:x1, :x2])
 
@@ -10,16 +10,35 @@ p = 8
 Ψ = PolynomialChaosBasis(LegendreBasis.([p, p]), p)
 
 # Estimation by least squares
-ls = LeastSquares(SobolSampling(1000))
-pce, samples, mse = polynomialchaos(x, model, Ψ, :y, ls)
+ls_n = 1000
+ls = LeastSquares(SobolSampling(ls_n))
+pceLS, samples, mse = polynomialchaos(x, model, Ψ, :y, ls)
 
-println("LS Mean: $(mean(pce))")
-println("LS Variance: $(var(pce))")
+println("LS Mean: $(mean(pceLS))")
+println("LS Variance: $(var(pceLS))")
 println("LS Mean Squared Error: $mse")
+println("--------------------------")
 
 # Estimation by full quadrature
 gq = GaussQuadrature()
-pce, samples = polynomialchaos(x, model, Ψ, :y, gq)
+pceGQ, samples = polynomialchaos(x, model, Ψ, :y, gq)
 
-println("GQ Mean: $(mean(pce))")
-println("GQ Variance: $(var(pce))")
+println("GQ Mean: $(mean(pceGQ))")
+println("GQ Variance: $(var(pceGQ))")
+println("--------------------------")
+
+# Estimation by MC
+sampsMC = sample(x, 10^6)
+evaluate!(model, sampsMC)
+
+println("MC Mean: $(mean(sampsMC[!,:y]))")
+println("MC Variance: $(var(sampsMC[!,:y]))")
+
+
+# Plots 3 histograms
+sampsLS = sample(pceLS, 10^5)
+sampsGQ = sample(pceGQ, 10^5)
+
+histogram(sampsLS[!,:y], alpha = 0.5, label = "Least squares", normalize = :probability, bins = 100)
+histogram!(sampsGQ[!,:y], alpha = 0.5, label = "Quadrature", normalize = :probability, bins = 100)
+histogram!(sampsMC[!,:y], alpha = 0.5, label = "Monte Carlo", normalize = :probability, bins = 100)
