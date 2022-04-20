312a1387aba19f5c019b52166d7d40bc43fa8c0d
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2022-04-20 15:52:27 +0200

    Update tests to include hermite basis

--- test/models/pce/polynomialchaosexpansion.jl
+++ test/models/pce/polynomialchaosexpansion.jl
@@ -1,26 +1,27 @@
 @testset "PolynomialChaosExpansion" begin
-    x = RandomVariable.(Uniform(-1, 1), [:x1, :x2])
+    x = RandomVariable.([Uniform(-2, 0), Normal(-1, 0.5), Uniform(0, 1)], [:x1, :x2, :x3])
 
     model = Model(df -> begin
-        π .* (df.x1 .- 1) .* sin.(π .* df.x1) .* (1 .- df.x2 .^ 2)
+        return df.x1 .+ df.x2 .* df.x3
     end, :y)
 
     p = 8
-    Ψ = PolynomialChaosBasis(LegendreBasis.([p, p]), p)
+    Ψ = PolynomialChaosBasis([LegendreBasis(p), HermiteBasis(p), LegendreBasis(p)], p)
 
     @testset "LeastSquares" begin
         ls = LeastSquares(SobolSampling(1000))
         pce, _, _ = polynomialchaos(x, model, Ψ, :y, ls)
 
-        @test mean(pce) ≈ 2 / 3 rtol = 0.001
-        @test var(pce) ≈ 2.931407384059896 rtol = 0.001
+        @test mean(pce) ≈ -1.5 rtol = 1e-10
+        @test var(pce) ≈ 0.5 rtol = 1e-10
     end
 
     @testset "GaussQuadrature" begin
         gq = GaussQuadrature()
         pce, _ = polynomialchaos(x, model, Ψ, :y, gq)
-        @test mean(pce) ≈ 2 / 3
-        @test var(pce) ≈ 2.9313966486926053
+
+        @test mean(pce) ≈ -1.5 rtol = 1e-10
+        @test var(pce) ≈ 0.5 rtol = 1e-10
     end
 
     @testset "evaluate" begin
