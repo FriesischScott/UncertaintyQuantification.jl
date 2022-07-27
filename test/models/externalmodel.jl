37da4464556850e5bb1f5b76e775d9c6a17d930d
Andrea Perin <andrea.perin1992@gmail.com>
2022-07-27 12:47:48 +0200

    Enable the possibility of running ```evaluate!``` of an ExternalModel with a 'cleanup` boolean flag to delete all the files generated by external solvers

--- test/models/externalmodel.jl
+++ test/models/externalmodel.jl
@@ -1,11 +1,11 @@
 @testset "ExternalModel" begin
     sourcedir = tempdir()
     sourcefiles = ["in.txt"]
-    extrafiles = []
+    extrafiles = String[]
 
     numberformats = Dict(:x => FormatSpec(".8e"), :* => FormatSpec(".8e"))
 
-    workdir = joinpath(tempdir(), "external-model-test")
+    workdir = map(i->(joinpath(tempdir(), "external-model-test-$(i)"), i), [true, false])
 
     r = Extractor(
         base -> begin
@@ -24,9 +24,9 @@
 
     opensees = Solver(binary, "", "in.txt")
 
-    ext = ExternalModel(
-        sourcedir, sourcefiles, extrafiles, numberformats, workdir, [r], opensees
-    )
+    ext = map(ii->ExternalModel(
+        sourcedir, sourcefiles, extrafiles, numberformats, ii[1], [r], opensees, ii[2]
+    ), [workdir[1], workdir[2]])
 
     open(joinpath(sourcedir, "in.txt"), "w") do input
         println(input, "{{{ :x }}}")
@@ -38,7 +38,14 @@
 
     df = sample([x, y], 1)
 
-    evaluate!(ext, df)
-
+    for model in ext
+        evaluate!(model, df)
+        if model.cleanup
+            @test length(readdir(readdir(model.workdir, join=true)[1])) == 0
+        else
+            @test length(readdir(readdir(model.workdir, join=true)[1])) != 0
+        end
+    end
+ 
     @test isapprox(df.r, sqrt.(df.x .^ 2 + df.y .^ 2))
 end
