bc94807ae5e01e7eb0e78e62acbad685fffb5979
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2022-09-13 15:07:24 +0200

    Refactoring

--- src/reliability/form.jl
+++ src/reliability/form.jl
@@ -15,26 +15,24 @@ function probability_of_failure(
 )
     # create reference point in standard normal space origin
     random_names = names(filter(i -> isa(i, RandomUQInput), inputs))
-    y = DataFrame(zeros(1, length(random_names)), :auto)
-    rename!(y, random_names)
+    y::Vector{Float64} = zeros(length(random_names))
 
     G = [models..., Model(performance, :performance)]
 
     deterministic_inputs = filter(i -> isa(i, DeterministicUQInput), inputs)
-    parameters = !isempty(deterministic_inputs) ? sample(deterministic_inputs, 1) : DataFrame()
+    parameters =
+        !isempty(deterministic_inputs) ? sample(deterministic_inputs, 1) : DataFrame()
 
     β::Float64 = 0.0
     h₀::Float64 = Inf
 
     # compute pf through HLRF algorithm
-    for it = 1 : sim.n
-        physical = copy(y)
+    for it in 1:(sim.n)
+        physical = DataFrame(random_names .=> y)
         to_physical_space!(inputs, physical)
         physical = hcat(physical, parameters)
 
-        H = gradient_in_standard_normal_space(
-            G, inputs, physical, :performance
-        )
+        H = gradient_in_standard_normal_space(G, inputs, physical, :performance)
 
         H = map(n -> H[n], random_names)
 
@@ -42,24 +40,27 @@ function probability_of_failure(
 
         h = physical[1, :performance]
 
-        α = -H ./ norm(H)
+        α = H ./ norm(H)
 
         if it == 1
-            h₀ = physical[1, :performance]
+            h₀ = h
         end
 
-        β = (h - (dot(H, Matrix(y)))) / norm(H)
+        β = norm(y)
 
-        Δ = norm(Matrix(y) .- (α .* β)')
-        y[:, :] = (α .* β)'
+        y_next = -α * (β + (h / norm(H)))
+        Δ = norm(y - y_next)
 
         if Δ < sim.tol
             break
         end
+
+        y = y_next
     end
 
     pf = cdf(Normal(), (h₀ > 0 ? -1 : 1) * β)
-    to_physical_space!(inputs, y)
+    dp = DataFrame(random_names .=> y)
+    to_physical_space!(inputs, dp)
 
-    return pf, β, y
+    return pf, β, dp
 end
