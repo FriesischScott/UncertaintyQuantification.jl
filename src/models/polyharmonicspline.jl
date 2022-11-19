ff0eb3d2822c73d2dfc364bb3d4a01ba935e3846
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2022-11-19 17:55:47 +0800

    Fix spacing for doctest

--- src/models/polyharmonicspline.jl
+++ src/models/polyharmonicspline.jl
@@ -73,11 +73,11 @@ Evaluate given data using a previously contructed PolyharmonicSpline metamodel.
 
 #Examples
 ```jldoctest
-julia>data = DataFrame(x = 1:10, y = [1, -5, -10, -12, -8, -1, 5, 12, 23, 50]);
-julia>ps = PolyharmonicSpline(data, 2, :y);
-julia>df = DataFrame( x = [2.5, 7.5, 12, 30]);
-julia>evaluate!(ps, df);
-julia>df.y
+julia> data = DataFrame(x = 1:10, y = [1, -5, -10, -12, -8, -1, 5, 12, 23, 50]);
+julia> ps = PolyharmonicSpline(data, 2, :y);
+julia> df = DataFrame( x = [2.5, 7.5, 12, 30]);
+julia> evaluate!(ps, df);
+julia> df.y
 4-element Vector{Float64}:
   -7.754272281066534
    8.290831024829075
