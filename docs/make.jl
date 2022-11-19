0a061cc4426cbb2d71dc7fafc6496f98967349fc
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2022-11-19 17:34:27 +0800

    Load DataFrames during meta

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ using Documenter, UncertaintyQuantification
 DocMeta.setdocmeta!(
     UncertaintyQuantification,
     :DocTestSetup,
-    :(using UncertaintyQuantification, Random; Random.seed!(8128));
+    :(using UncertaintyQuantification, Random, DataFrames; Random.seed!(8128));
     recursive=true,
 )
 
