97075ba7566c47a5be55f222b5c69ceaf5b856d0
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2023-02-27 16:03:18 +0100

    Fix doctest for PolyharmonicSpline

--- src/models/polyharmonicspline.jl
+++ src/models/polyharmonicspline.jl
@@ -8,7 +8,7 @@ Creates a polyharmonic spline that is trained by given data.
 julia> data = DataFrame(x = 1:10, y = [1, -5, -10, -12, -8, -1, 5, 12, 23, 50]);
 
 julia> PolyharmonicSpline(data, 2, :y) |> DisplayAs.withcontext(:compact => true)
-PolyharmonicSpline([1.14733; -0.449609; … ; -5.33101; 3.88628;;], [-112.005; 6.84443;;], [1.0; 2.0; … ; 9.0; 10.0;;], 2, [:x], :y)
+PolyharmonicSpline([1.14733, -0.449609, 0.0140379, -1.02859, -0.219204, 0.900367, 0.00895592, 1.07145, -5.33101, 3.88628], [-112.005, 6.84443], [1.0; 2.0; … ; 9.0; 10.0;;], 2, [:x], :y)
 ```
 """
 struct PolyharmonicSpline <: UQModel
