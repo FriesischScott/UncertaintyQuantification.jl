841db5ddf5a8236a81098bab58c3d48b73be9825
Andrea Perin <andrea.perin1992@gmail.com>
2023-07-19 16:22:17 +0200

    Add a check on solver path.
            - stdout and stderr from solver run do not depend anymore on
              relative paths.
            - a warning will be throw when he solver has a relative path

--- test/solvers/solvers.jl
+++ test/solvers/solvers.jl
@@ -25,7 +25,12 @@
 
     current_dir = pwd()
 
-    @test_throws Base.IOError run(Solver("this/solver/does/not/exist", "in.txt"), tmp)
+    @test_warn "Specified solver path is not an absolute path. Make sure this_path_is_not_absolute is in your environmental varibles path." Solver(
+        "this_path_is_not_absolute", "in.txt"
+    )
 
+    @test_throws Base.IOError run(
+        Solver(joinpath(pwd(), "this/solver/does/not/exist"), "in.txt"), tmp
+    )
     @test pwd() == current_dir
 end
