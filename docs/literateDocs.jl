c5cced067f6ceee30c3f92a507444008aaa8900b
mlsuh <max-luttmann@stud.uni-hannover.de>
2023-09-13 14:34:24 +0200

    Refine literateDocs.jl and Documentation.yml
    Remove meta-block containing edit-URL (genereated by litereate)
    from md-files.

--- docs/literateDocs.jl
+++ docs/literateDocs.jl
@@ -1,13 +1,22 @@
 using Literate
 
 function fuseConvert(path::String, out::IOStream, r::String, dir::String)
-    for (root, dirs, files) in walkdir(joinpath(r, dir))
+    for (root, _, files) in walkdir(joinpath(r, dir))
         for file in files
             write(path, read(joinpath(root, file), String) * "\n\n")
         end
     end
     close(out)
     Literate.markdown(path, "./docs/src/examples"; documenter=true, name=dir)
+
+    #remove @meta block created by literate
+    s = read("./docs/src/examples/" * dir * ".md", String)
+    lines = split(s, "\n"; limit=4)
+    open("./docs/src/examples/" * dir * ".md", "w") do f
+        write(f, lines[4])
+        close(f)
+    end
+
     return nothing
 end
 
@@ -17,3 +26,21 @@ for (r, d, f) in walkdir("./docs/literate/")
         fuseConvert(tmpPath, tmpFile, r, dir)
     end
 end
+
+# out = ""
+#         for (root, dirs, files) in walkdir(joinpath(r, dir))
+#             for file in files
+#                 if (cmp(file, dir * ".jl") != 0)
+#                     out = out * read(joinpath(root, file), String) * "\n\n"
+#                 end
+#             end
+#         end
+
+#         open(joinpath(joinpath(r, dir), dir * ".jl"), "w") do f
+#             write(f, out)
+#             close(f)
+#         end
+
+#         Literate.markdown(
+#             joinpath(joinpath(r, dir), dir * ".jl"), "./docs/src/examples"; documenter=true
+#         )
