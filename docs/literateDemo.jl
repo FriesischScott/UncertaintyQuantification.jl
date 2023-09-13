29648592f15246ac96f021574a67ba45324093de
mlsuh <max-luttmann@stud.uni-hannover.de>
2023-09-13 13:38:27 +0200

    Refine doc generation and add demo update workflow
    demo files will be generated separately while markdown files
    will contain all examples of a certain "super-topic". This helps fit the
    limited hierarchy depths available for documentation.
    
    The DemoFiles worklfow will ensure that demo files fit the
    corresponding literate script which blocks manual changes

--- docs/literateDemo.jl
+++ docs/literateDemo.jl
@@ -1,14 +1,10 @@
 using Literate
 
 for (root, dirs, files) in walkdir("./docs/literate/")
+    sp = splitpath(root)
     if (!isempty(files))
-        sp = splitpath(root)
-        if (cmp(sp[end], "literate") == 0)
-            Literate.script.(joinpath.(root, files), joinpath.(pwd(), "./demo"))
-        else
-            Literate.script.(
-                joinpath.(root, files), joinpath.(pwd(), "./demo/", joinpath(sp[4:end]))
-            )
-        end
+        Literate.script.(
+            joinpath.(root, files), joinpath.(pwd(), "./demo/", joinpath(sp[4:end]))
+        )
     end
 end
