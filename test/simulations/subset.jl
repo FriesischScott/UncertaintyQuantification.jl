8a95dbf37ce76055f0c28d3704465ada091ad17f
Andrea Perin <andrea.perin1992@gmail.com>
2023-09-13 17:00:35 +0200

    Fix constraints for SubSetSimulation's proposal pdf (#132)
    Closes #130.

--- test/simulations/subset.jl
+++ test/simulations/subset.jl
@@ -11,4 +11,10 @@
     @test_throws ErrorException("proposal must be a symmetric distribution") SubSetSimulation(
         2000, 0.2, 10, Exponential()
     )
+    @test_throws ErrorException("proposal must be centered in 0") SubSetSimulation(
+        2000, 0.2, 10, Uniform()
+    )
+    @test_logs (:warn, "A proposal pdf with large variance (≥ 2) can be inefficient.") SubSetSimulation(
+        2000, 0.2, 10, Uniform(-4, 4)
+    )
 end
