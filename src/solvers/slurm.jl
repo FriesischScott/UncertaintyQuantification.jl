f4ba3cb05ca9044d04415985d9c5e92e7d86a578
Ander Gray <ander.gray@ukaea.uk>
2024-02-06 16:10:15 +0000

    format code

--- src/solvers/slurm.jl
+++ src/solvers/slurm.jl
@@ -1,8 +1,7 @@
 ## Probably better to make a 'SlurmSolver' version of 'solver'
 
 struct SlurmInterface
-
-    name::String        # Does nothing
+    name::String
     account::String
     partition::String
     nodes::Integer
@@ -11,23 +10,21 @@ struct SlurmInterface
     extras::Vector{String}
     time::String
 
-    function SlurmInterface(;name::String, 
+    function SlurmInterface(;
+        name::String,
         account::String,
         partition::String,
         nodes::Integer,
         ntasks::Integer,
         batchsize::Integer,
         extras::Vector{String},
-        time::String)
-
+        time::String,
+    )
         return new(name, account, partition, nodes, ntasks, batchsize, extras, time)
     end
-
 end
 
 function run_slurm_array(SI, m, n, path)
-    #slurm_array.sh
-
     binary = m.solver.path
     source = m.solver.source
     args = m.solver.args
@@ -41,36 +38,34 @@ function run_slurm_array(SI, m, n, path)
     fname = "slurm_array.sh"
     dirpath = joinpath(m.workdir, path)
     fpath = joinpath(dirpath, fname)
-    
+
     open(fpath, "w") do file
         write(file, "#!/bin/bash -l\n")
         write(file, "#SBATCH -A $(SI.account)\n")
         write(file, "#SBATCH -p $(SI.partition)\n")
-        write(file, "#SBATCH -J UQ_array\n")
+        write(file, "#SBATCH -J $(SI.name)\n")
         write(file, "#SBATCH --nodes=$(SI.nodes)\n")
         write(file, "#SBATCH --ntasks=$(SI.ntasks)\n")
-        write(file, "#SBATCH --time=$(time)\n")
+        write(file, "#SBATCH --time=$(SI.time)\n")
         write(file, "#SBATCH --output=UncertaintyQuantification_%A-%a.out\n")
         write(file, "#SBATCH --error=UncertaintyQuantification_%A-%a.err\n")
         write(file, "#SBATCH --array=[1-$(n)]%$(SI.batchsize)\n")
-        write(file,"\n\n\n")
-        write(file,"#### EXTRAS ####\n")
+        write(file, "\n\n\n")
+        write(file, "#### EXTRAS ####\n")
 
         for extra in extras
             write(file, "$extra\n")
         end
 
-        write(file,"\n\n\n")
-        write(file,"#### RUN COMMAND ####\n")
-        write(file, "cd 'sample-%0$(digits)d' \$SLURM_ARRAY_TASK_ID\n")       ## Slurm array task not working
+        write(file, "\n\n\n")
+        write(file, "#### RUN COMMAND ####\n")
+        write(file, "cd 'sample-%0$(digits)d' \$SLURM_ARRAY_TASK_ID\n")
         write(file, "$run_command\n")
     end
 
-    p = pipeline(
-        Cmd(["sbatch --wait slurm_array.sh]"])
-    )
-    cd(() -> run(p), path)
-    # cd(() -> run(p))
+    p = pipeline(`sbatch --wait slurm_array.sh`)
+
+    cd(() -> run(p), dirpath)
 
     return nothing
 end
