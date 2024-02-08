a4b043fb6029561479485c9450dd0cabd98c5af3
Thomas Potthast <potthast@irz.uni-hannover.de>
2024-02-08 15:00:39 +0100

    Add two models, one with noise and one without. Generate observations and start likelihood

--- demo/bayesianinference/2dof.jl
+++ demo/bayesianinference/2dof.jl
@@ -1,17 +1,49 @@
 using UncertaintyQuantification
+using Plots
+using LaTeXStrings
 
-function example2dof(θ::AbstractArray)
-    λ1_noise = ((θ[1] + 2 * θ[2]) + sqrt(θ[1]^2 + 4 * θ[2]^2)) / 2 + rand(Normal())
-    λ2_noise = ((θ[1] + 2 * θ[2]) - sqrt(θ[1]^2 + 4 * θ[2]^2)) / 2 + rand(Normal(0, 0.5))
-    return [λ1_noise, λ2_noise]
+function analyticalexample_2D_noise(θ::AbstractArray)
+    λ1_model = ((θ[1] + 2 * θ[2]) + sqrt(θ[1]^2 + 4 * θ[2]^2)) / 2
+    λ2_model = ((θ[1] + 2 * θ[2]) - sqrt(θ[1]^2 + 4 * θ[2]^2)) / 2
+
+    λ1_noise = λ1_model + rand(Normal())
+    λ2_noise = λ2_model + rand(Normal(0, 0.5))
+    return [λ1_noise λ2_noise]
 end
 
-function likelihood(λ_noisy::AbstractArray, λ_model::AbstractArray)
-    for dim in 1:size(λ_model, 2)
-        for iobservation in 1:size(λ_noisy, 1)
-            #blablablabö
-        end
-    end
-
-    return [λ1_noise, λ2_noise]
+function analyticalexample_2D(θ::AbstractArray)
+    λ1_model = ((θ[1] + 2 * θ[2]) + sqrt(θ[1]^2 + 4 * θ[2]^2)) / 2
+    λ2_model = ((θ[1] + 2 * θ[2]) - sqrt(θ[1]^2 + 4 * θ[2]^2)) / 2
+    return [λ1_model λ2_model]
 end
+
+function likelihood(θ::AbstractArray, model::Function, Yexp::AbstractMatrix)
+    σ1 = 1
+    σ2 = 0.5
+    Ysim = model(θ)
+    temp_exp = -0.5 * (((Yexp[1] - Ysim[1]) / σ1)^2 + ((Yexp[1] - Ysim[1]) / σ1)^2)
+    return exp(temp_exp)
+end
+
+### True value
+λ_true = analyticalexample_2D([0.5 1.5])
+
+### Obervations
+N_obs = 15
+λ_obs = Matrix{Float64}(undef, N_obs, 2)
+
+for i_obs in 1:N_obs
+    λ_obs[i_obs, :] = analyticalexample_2D_noise([0.5 1.5])
+end
+
+### Figures
+scatter(
+    λ_obs[:, 1],
+    λ_obs[:, 2];
+    xlabel="Eigenvalue " * L"λ_1^{noisy}",
+    ylabel="Eigenvalue " * L"λ_2^{noisy}",
+    color="red",
+    label="Noisy eigenvalue",
+)
+
+scatter!([λ_true[1, 1]], [λ_true[1, 2]]; color="black", label="True eigenvalue", marker=:+)
