dedea898cba207da68ecf94806b8d8cba5802a2f
Ander Gray <ander.gray@ukaea.uk>
2024-02-09 14:53:11 +0000

    adds docstrings

--- src/hpc/slurm.jl
+++ src/hpc/slurm.jl
@@ -1,5 +1,38 @@
-## Probably better to make a 'SlurmSolver' version of 'solver'
+"""
+	SlurmInterface(account::String, partition::String, nodes::Integer, ntasks::Integer, batchsize::Integer, jobname::String, extras::Vector{String}, time::String)
 
+When `SlurmInterface` is passed to an `ExternalModel`, model evaluations are executed using slurm job arrays.
+This allows for heavier simulations or workflows to be sampled, without relying on Julia's native parallelism.
+`SlurmInterface` automatically generates a slurm job array script, and Julia waits for this job to finish before extracting results.
+
+When using `SlurmInterface`, you no longer need to load workers into Julia with `addprocs(N)`, and the requested nodes / tasks those 
+required by individual model evaluations. Use `extras` to specify anything that must be preloaded for your models to be executed (for example loading modules).
+
+The `batchsize` specifies the number of simulations in the job array which are run concurrently. I.e., if you perform 
+`MonteCarlo` simulation with `N=1000` samples, with `batchsize=200`, it will run 1000 simulations in total, but only 200 at the same time. 
+Your HPC scheduler (and admin) may be unhappy if you request too many concurrent jobs.
+
+
+# parameters
+
+    account   : the account to charge the computing time to
+    partition : the partition to use
+    nodes     : number of nodes per job
+    ntasks    : total number of cores
+    batchsize : the number of jobs to be run at the same time
+    jobname   : name of the job
+    extras    : instructions to be executed before the model is run, e.g. activating a python environment
+    time      : how long each job takes, e.g. 00:10:00 for 10 minutes per sample
+
+# Examples
+```jldoctest
+julia> slurm = SlurmInterface(account = "HPC_account_1", partition = "CPU_partition", nodes = 1, ntasks = 32, batchsize = 200, extras = ["load python3"], time = "00:10:00")
+SlurmInterface("HPC_account_1", "CPU_partition", 1, 32, 200, "UQ_array", ["load python3"], "00:10:00")
+
+
+```
+
+"""
 struct SlurmInterface <: AbstractHPCScheduler
     account::String
     partition::String
@@ -18,7 +51,7 @@ struct SlurmInterface <: AbstractHPCScheduler
         batchsize::Integer,
         jobname::String,
         extras::Vector{String},
-        time::String
+        time::String,
     )
         return new(account, partition, nodes, ntasks, batchsize, jobname, extras, time)
     end
@@ -30,11 +63,13 @@ function SlurmInterface(;
     nodes::Integer,
     ntasks::Integer,
     batchsize::Integer,
-    jobname::String = "UQ_array",
-    extras::Vector{String} = String[],
-    time::String = ""
+    jobname::String="UQ_array",
+    extras::Vector{String}=String[],
+    time::String="",
 )
-    return SlurmInterface(account, partition, nodes, ntasks, batchsize, jobname, extras, time)
+    return SlurmInterface(
+        account, partition, nodes, ntasks, batchsize, jobname, extras, time
+    )
 end
 
 function run_slurm_array(SI, m, n, path)
