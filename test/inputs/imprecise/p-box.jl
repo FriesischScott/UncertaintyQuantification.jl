a49e734acba8b308573b42937c3898e77a21c738
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2024-02-23 17:02:06 +0100

    Add tests for pbox with parameters

--- test/inputs/imprecise/p-box.jl
+++ test/inputs/imprecise/p-box.jl
@@ -12,6 +12,8 @@
     @test p_box.parameters == params
     @test p_box.name == name
 
+    @test UncertaintyQuantification.bounds(p_box) == ([0.14, 0.21], [0.16, 0.23])
+
     par = [0.13, 0.20]
     @test_throws ErrorException(
         "Values outside of parameter intervals for ProbabilityBox l."
@@ -40,4 +42,19 @@
     a, b = UncertaintyQuantification.sample(p_box, 0.75)
     @test a == quantile(Normal(0, 0.1), 0.75)
     @test b == quantile(Normal(1, 1), 0.75)
+
+    p_box = ProbabilityBox{Normal}([Parameter(0, :μ), Interval(0.1, 1, :σ)], name)
+    @test UncertaintyQuantification.bounds(p_box) == ([0.1], [1])
+
+    a, b = UncertaintyQuantification.sample(p_box, 0.25)
+    @test a == quantile(Normal(0, 1), 0.25)
+    @test b == quantile(Normal(0, 0.1), 0.25)
+
+    a, b = UncertaintyQuantification.sample(p_box, 0.5)
+    @test a == 0.0
+    @test b == 0.0
+
+    a, b = UncertaintyQuantification.sample(p_box, 0.75)
+    @test a == quantile(Normal(0, 0.1), 0.75)
+    @test b == quantile(Normal(0, 1), 0.75)
 end
