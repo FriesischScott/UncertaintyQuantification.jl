59aa7bc546aaaaab5bedef17e7f1a47a5750d35f
Ander Gray <ander.gray@ukaea.uk>
2024-05-03 15:28:36 +0100

    updates doc strings for SubSet Adaptive

--- src/simulations/subset.jl
+++ src/simulations/subset.jl
@@ -94,21 +94,28 @@ end
 
 Implementation of: Papaioannou, Iason, et al. "MCMC algorithms for subset simulation." Probabilistic Engineering Mechanics 41 (2015): 89-103
 
-Defines the properties of a Subset-∞ adaptive where `n` is the number of initial samples,
+Defines the properties of a Subset-∞ adaptive where `n` are the number of samples per level,
 `target` is the target probability of failure at each level, `levels` is the maximum number
-of levels and `λ` (λ = 1 recommended) is the initial scaling parameter and `Na` is the number of
-times to update `λ` per subset level (number of partitions of seeds). The initial variance of the proposal distribution is `λ`.
+of levels, `λ` (λ = 1 recommended) is the initial scaling parameter, and `Na` is the number simulations that will be run before `λ` 
+is updated. Note that Na must be a multiple of n * target: `mod(ceil(n * target), Na) == 0)`. The initial variance of the proposal distribution is `s`.
 
 
 Idea behind this algorithm is to adaptively select the correlation parameter of `s`
-at each intermediate level, by simulating a subset N_a of the chains
+at each intermediate level, by simulating a subset Na of the chains
 (which must be choosen without replacement at random) and modifying the acceptance rate towards the optiming
-α_star = 0.44
+αstar = 0.44
 
 # Constructors
 * `SubSetInfinityAdaptive(n::Integer, target::Float64, levels::Integer, Na::Integer)`   (default: λ = s = 1)
 * `SubSetInfinityAdaptive(n::Integer, target::Float64, levels::Integer, Na::Integer, λ::Real)` (λ = s)
 * `SubSetInfinityAdaptive(n::Integer, target::Float64, levels::Integer, Na::Integer, λ::Real, s::Real)`
+
+# Note
+The following constructors will run the same number of samples, but SubSetInfinityAdaptive will update `s` after each chain:
+
+* `SubSetInfinityAdaptive(400, 0.1, 10, 40)`
+* `SubSetInfinity(400, 0.1, 10, 0.5)`
+
 
 # Examples
 
