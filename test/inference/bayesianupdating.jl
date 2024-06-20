d54c67ead86fa31942cd618462f8aae41bb3316d
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2024-06-20 17:27:09 +0200

    Add test for TMCMC

--- test/inference/bayesianupdating.jl
+++ test/inference/bayesianupdating.jl
@@ -14,4 +14,21 @@
         @test mean(mh_samples.x) ≈ 1.6 rtol = 0.05
         @test std(mh_samples.x) ≈ 0.45 rtol = 0.05
     end
+
+    @testset "Transitional Markov Chain Monte Carlo" begin
+        prior_sample = [RandomVariable(Normal(), :x)]
+        prior(df) = logpdf(Normal(), df.x)
+
+        likelihood(df) = logpdf(Normal(2, 0.5), df.x)
+
+        n = 1000
+        burnin = 100
+
+        tmcmc = TransitionalMarkovChainMonteCarlo(prior_sample, n, burnin, 0.2)
+
+        tmcmc_samples, _ = bayesianupdating(prior, likelihood, UQModel[], tmcmc)
+
+        @test mean(tmcmc_samples.x) ≈ 1.6 rtol = 0.05
+        @test std(tmcmc_samples.x) ≈ 0.45 rtol = 0.05
+    end
 end
