f38f02897226d28e2cf1341c45e2c4c0b2408634
Ander Gray <ander.gray@ukaea.uk>
2024-06-26 15:48:16 +0100

    expand on docstring of bayesianupdating

--- src/modelupdating/bayesianupdating.jl
+++ src/modelupdating/bayesianupdating.jl
@@ -44,6 +44,20 @@ When using [`TransitionalMarkovChainMonteCarlo`](@ref) the `prior` can automatic
 
     bayesinupdating(likelihood, models, tmcmc)
     bayesianupdating(likelihood, tmcmc)
+
+
+### Notes
+
+`likelihood` is a Julia function which must be defined in terms of a `DataFrame` of samples, and must evaluate the likelihood for each row of the `DataFrame`
+
+For example, a loglikelihood based on normal distribution using 'Data':
+
+```julia
+likelihood(df) = [sum(logpdf.(Normal.(df_i.x, 1), Data)) for df_i in eachrow(df)]
+```
+
+If a model evaluation is required to evaluate the likelihood, a vector of `UQModel`s must be passed to `bayesianupdating`. For example if the variable `x` above is the output of a numerical model.
+
 """
 function bayesianupdating(
     prior::Function,
