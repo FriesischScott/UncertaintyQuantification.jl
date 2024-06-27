97f51ad48ff6f6547f6f73a32c9964c9bff21126
Ander Gray <ander.gray@ukaea.uk>
2024-06-27 11:50:50 +0100

    update BMU example docs

--- docs/literate/bayesianupdating/inverse-eigenvalue.jl
+++ docs/literate/bayesianupdating/inverse-eigenvalue.jl
@@ -12,7 +12,7 @@ The inverse eigenvalue problem is a classic engineering example. Here we will us
 \end{bmatrix}
 ```
 
-A matrix of this form can represent different problems, like the stiffness matrix describing a tuned mass damper system. In this example we assume the fixed values $\theta_11$ = 0.5 and $\theta_2$ = 1.5 for the variables.
+A matrix of this form can represent different problems, like the stiffness matrix describing a tuned mass damper system. In this example we assume the fixed values $\theta_1$ = 0.5 and $\theta_2$ = 1.5 for the variables.
 
 The eigenvalues $\lambda_1$ and $\lambda_2$ of this matrix represent a physical measurable property corrupted by "noise" created for example due to environmental factors or measurement inaccuracy.
 
@@ -24,7 +24,7 @@ The eigenvalues $\lambda_1$ and $\lambda_2$ of this matrix represent a physical 
 \lambda_2^{noisy} = \frac{(\theta_1+2\theta_2)-\sqrt{\theta_1^2+4{\theta_2}^2}}{2} + \epsilon_2
 ```
 
-The "noise" terms \$\epsilon_1$ and $\epsilon_2$ follow a Normal distribution with zero mean and standard deviations $\sigma_1$ = $1.0$ and $\sigma_2$ = $0.1$.
+The "noise" terms $\epsilon_1$ and $\epsilon_2$ follow a Normal distribution with zero mean and standard deviations $\sigma_1$ = $1.0$ and $\sigma_2$ = $0.1$.
 
  The synthetic "noisy" data used for the Bayesian updating procedure is given in the following table.
 
@@ -46,7 +46,7 @@ The "noise" terms \$\epsilon_1$ and $\epsilon_2$ follow a Normal distribution wi
 |  3.58     |    0.25   |
 |  2.62     |    0.25   |
 
-The a priori knowledge of $\theta_1$ and $\theta_2$ is that they take values between 0.01 and 4. The likelihood function used for this problem is a bivariate Gaussian function with the off-diagonal terms of the covariance matrix equal to 0 and the diagonal terms corresponding to the standard deviation of the respective noise terms.
+The a priori knowledge of $\theta_1$ and $\theta_2$ is that they take values between 0.01 and 4. The likelihood function used for this problem is a bivariate Gaussian function with a covariance matrix $\begin{bmatrix} \sigma_1 & 0 \\ 0 & \sigma_2 \end{bmatrix}$, with off-diagonal terms equal to 0 and the diagonal terms corresponding to the standard deviation of the respective noise terms.
 
 ```math
 P(\lambda|\theta) \propto \exp \left[-\frac{1}{2}\sum_{i=1}^2\sum_{n=1}^{15} {\left(\frac{\lambda_{i,n}^{data}-\lambda_i^{model}}{\sigma_i}\right)}^2\right]
@@ -90,7 +90,7 @@ function likelihood(df)
     return log.(exp.([-0.5 * sum(((Y .- λ[n, :]') ./ σ) .^ 2) for n in axes(λ, 1)]))
 end
 
-# We will solve this problem using the TMCMC algorithm. Therefore,the next step is to define the [`RandomVariable`](@ref) vector of the prior, followed by the [`TransitionalMarkovChainMonteCarlo`](@ref) object.
+# We will solve this problem using the TMCMC algorithm. Therefore, the next step is to define the [`RandomVariable`](@ref) vector of the prior, followed by the [`TransitionalMarkovChainMonteCarlo`](@ref) object.
 
 prior = RandomVariable.(Uniform(0.01, 4), [:θ1, :θ2])
 
@@ -105,4 +105,4 @@ samples, evidence = bayesianupdating(likelihood, [λ1, λ2], tmcmc)
 
 scatter(samples.θ1, samples.θ2; lim=[0, 4], label="TMCMC", xlabel="θ1", ylabel="θ2")
 
-#  A scatter plot of the resulting samples shows convergence to two distinct points. In difference to the transitional Markov Chain Monte Carlo algorithm, the standard Metropolis-Hastings algorithm would have only identified one of the two points.
+#  A scatter plot of the resulting samples shows convergence to two distinct regions. Unlike the transitional Markov Chain Monte Carlo algorithm, the standard Metropolis-Hastings algorithm would have only identified one of the two regions.
