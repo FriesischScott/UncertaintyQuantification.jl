b860da5bf015ae9597665310f847b98a5f321f19
Ander Gray <ander.gray@ukaea.uk>
2024-07-12 09:37:47 +0100

    adds more tests for Double loop MC using analytical results

--- test/reliability/probabilityoffailure_imprecise.jl
+++ test/reliability/probabilityoffailure_imprecise.jl
@@ -8,8 +8,35 @@
                 UQModel[], df -> 9 .+ df.X .+ df.Y, [X, Y], DoubleLoop(FORM())
             )
 
-            @test pf.lb ≈ 0.0 atol = 1e-6
-            @test pf.ub ≈ 0.006664164390407847 atol = 1e-6
+            @test pf.lb ≈ cdf(Normal(2, sqrt(2)), -9) atol = 1e-6
+            @test pf.ub ≈ cdf(Normal(-2, sqrt(8)), -9) atol = 1e-6
         end
+
+        @testset "Interval - Distribution" begin
+
+            X = Interval(-1, 1, :X)
+            Y = RandomVariable(Normal(0, 2), :Y)
+
+            pf = probability_of_failure(
+                UQModel[], df -> 9 .+ df.X .+ df.Y, [X, Y], DoubleLoop(FORM())
+            )
+
+            @test pf.lb ≈ cdf(Normal(1, 2), -9) atol = 1e-6
+            @test pf.ub ≈ cdf(Normal(-1, 2), -9) atol = 1e-6
+        end
+
+        @testset "Interval - p-box" begin
+            
+            X = Interval(-1, 1, :X)
+            Y = ProbabilityBox{Normal}([Parameter(0, :μ), Interval(1, 2, :σ)], :Y)
+
+            pf = probability_of_failure(
+                UQModel[], df -> 9 .+ df.X .+ df.Y, [X, Y], DoubleLoop(FORM())
+            )
+
+            @test pf.lb ≈ cdf(Normal(1, 1), -9) atol = 1e-6
+            @test pf.ub ≈ cdf(Normal(-1, 2), -9) atol = 1e-6
+        end
+
     end
 end
