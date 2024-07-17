d571095efc6523f048addb969959dc001e07d7d8
Jasper Behrensdorf <jasper@behrensdorf.de>
2024-07-17 12:55:45 +0200

    Discard changes to src/reliability/probabilityoffailure.jl

--- src/reliability/probabilityoffailure.jl
+++ src/reliability/probabilityoffailure.jl
@@ -22,26 +22,23 @@ function probability_of_failure(
     sim::LineSampling,
 )
     if isempty(sim.direction)
-        direction = gradient_in_standard_normal_space(
-            wrap(models),
+        sim.direction = gradient_in_standard_normal_space(
+            [models..., Model(x -> -1 * performance(x), :performance)],
             inputs,
-            DataFrame(names(inputs) .=> sns_zero_point(inputs)),
-            x -> -1 * performance(x),
+            DataFrame(names(inputs) .=> mean(inputs)),
+            :performance,
         )
-        sim_next = LineSampling(sim.lines, sim.points, direction)
-    else
-        sim_next = deepcopy(sim)
     end
 
-    samples = sample(inputs, sim_next)
+    samples = sample(inputs, sim)
     evaluate!(models, samples)
 
-    p = reshape(performance(samples), length(sim.points), sim_next.lines)
+    p = reshape(performance(samples), length(sim.points), sim.lines)
 
     ϕ = Normal()
-    ξ = zeros(sim_next.lines)
-    x = median(sim_next.points)
-    for i in 1:(sim_next.lines)
+    ξ = zeros(sim.lines)
+    x = median(sim.points)
+    for i in 1:(sim.lines)
         if all(p[:, i] .< 0)
             ξ[i] = 1.0
             @warn "All samples for line $i are inside the failure domain"
@@ -51,7 +48,7 @@ function probability_of_failure(
             @warn "All samples for line $i are outside the failure domain"
             continue
         end
-        spl = Spline1D(sim_next.points, p[:, i])
+        spl = Spline1D(sim.points, p[:, i])
         try
             root = Dierckx.roots(spl)[1]
             ξ[i] = cdf.(ϕ, -root)
@@ -61,7 +58,7 @@ function probability_of_failure(
     end
 
     pf = mean(ξ)
-    variance = var(ξ) / sim_next.lines
+    variance = var(ξ) / sim.lines
 
     return pf, sqrt(variance), samples
 end
@@ -72,21 +69,14 @@ function probability_of_failure(
     inputs::Union{Vector{<:UQInput},UQInput},
     sim::ImportanceSampling,
 )
-    if isempty(sim.dp) || isempty(sim.α)
-        _, β, dp, α = probability_of_failure(models, performance, inputs, FORM())
-        sim_next = ImportanceSampling(sim.n, β, dp, α; c=sim.c)
-    else
-        sim_next = deepcopy(sim)
-    end
-
-    samples, weights = sample(inputs, sim_next)
+    samples, weights = sample(inputs, sim)
     evaluate!(models, samples)
 
     # Probability of failure
     weighted_failures = (performance(samples) .< 0) .* weights
-    pf = sum(weighted_failures) / sim_next.n
+    pf = sum(weighted_failures) / sim.n
 
-    variance = ((sum(weighted_failures .* weights) / sim_next.n) - pf^2) / sim_next.n
+    variance = ((sum(weighted_failures .* weights) / sim.n) - pf^2) / sim.n
 
     return pf, sqrt(variance), samples
 end
