7442f4f331cce6b0b3a5abb34d89186dbf4f30bf
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2024-09-26 12:17:08 +0200

    Update docstring

--- src/hpc/slurm.jl
+++ src/hpc/slurm.jl
@@ -6,13 +6,13 @@ When `SlurmInterface` is passed to an `ExternalModel`, model evaluations are exe
 When using `SlurmInterface`, you no longer need to load workers into Julia with `addprocs(N)`, and the requested nodes / tasks those required by individual model evaluations. Use `extras` to specify anything that must be preloaded for your models to be executed (for example loading modules).
 
 The `throttle` specifies the number of simulations in the job array which are run concurrently. I.e., if you perform
-`MonteCarlo` simulation with `N=1000` samples, with `throttle=200`, it will run 1000 simulations in total, but only 200 at the same time. Your HPC scheduler (and admin) may be unhappy if you request too many concurrent jobs. If left empty, you scheduler's default throttle will be used. If your scheduler restricts the maximum size of a slurm job array, you can use `batchsize` to split the work into smaller batches.
+`MonteCarlo` simulation with `N=1000` samples, with `throttle=200`, it will run 1000 simulations in total, but only 200 at the same time. Your HPC scheduler (and admin) may be unhappy if you request too many concurrent jobs. If left empty, you scheduler's default throttle will be used.  In the case that your HPC machine limits the size of submitted job arrays, you can split the submissions into smaller "batches". Specify "batchsize" to the maximum size of a job array. This does not change the total number of runs.
 
 # parameters
 
-    options   : SBATCH options to add to the slurm script
+    options   : A dictionary of SBATCH options to add to the slurm script
     throttle  : the number of jobs to be run at the same time
-    batchsize : maximum size of the slurm array
+    batchsize : maximum size of the slurm array,use when HPC limits the number of jobs in arrays
     extras    : instructions to be executed before the model is run, e.g. activating a python environment or loading modules
 
 # Examples
