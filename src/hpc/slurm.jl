195c39b0c7b7d3092344f2acf81385e537544517
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2024-09-26 12:52:19 +0200

    Pass slurm options as dict (#207)
    Significantly improves flexibility.

--- src/hpc/slurm.jl
+++ src/hpc/slurm.jl
@@ -1,44 +1,49 @@
 """
-	SlurmInterface(account::String, partition::String, nodes::Integer, ntasks::Integer, throttle::Integer, jobname::String, extras::Vector{String}, time::String)
+	SlurmInterface(options::Dict{String,String}, throttle::Integer, btachsize::Integer, extras::Vector{String})
 
 When `SlurmInterface` is passed to an `ExternalModel`, model evaluations are executed using slurm job arrays. This allows for heavier simulations or workflows to be sampled, without relying on Julia's native parallelism. `SlurmInterface` automatically generates a slurm job array script, and Julia waits for this job to finish before extracting results.
 
 When using `SlurmInterface`, you no longer need to load workers into Julia with `addprocs(N)`, and the requested nodes / tasks those required by individual model evaluations. Use `extras` to specify anything that must be preloaded for your models to be executed (for example loading modules).
 
 The `throttle` specifies the number of simulations in the job array which are run concurrently. I.e., if you perform
-`MonteCarlo` simulation with `N=1000` samples, with `throttle=200`, it will run 1000 simulations in total, but only 200 at the same time. Your HPC scheduler (and admin) may be unhappy if you request too many concurrent jobs. If left empty, you scheduler's default throttle will be used. If your scheduler restricts the maximum size of a slurm job array, you can use `batchsize` to split the work into smaller batches.
+`MonteCarlo` simulation with `N=1000` samples, with `throttle=200`, it will run 1000 simulations in total, but only 200 at the same time. Your HPC scheduler (and admin) may be unhappy if you request too many concurrent jobs. If left empty, you scheduler's default throttle will be used. In the case that your HPC machine limits the size of submitted job arrays, you can split the submissions into smaller "batches". Specify "batchsize" to the maximum size of a job array. This does not change the total number of runs.
 
 # parameters
 
-    account   : the account to charge the computing time to
-    partition : the partition to use
-    nodes     : number of nodes per job
-    ntasks    : total number of cores
+    options   : A dictionary of SBATCH options to add to the slurm script
     throttle  : the number of jobs to be run at the same time
-    batchsize : maximum size of the slurm array
-    jobname   : name of the job
-    mempercpu : string, amount of RAM to give per cpu (default MB)
-    extras    : instructions to be executed before the model is run, e.g. activating a python environment
-    time      : how long each job takes, e.g. 00:10:00 for 10 minutes per sample
+    batchsize : maximum size of the slurm array, use when HPC limits the number of jobs in arrays
+    extras    : instructions to be executed before the model is run, e.g. activating a python environment or loading modules
 
 # Examples
 ```jldoctest
-julia> slurm = SlurmInterface(account = "HPC_account_1", partition = "CPU_partition", nodes = 1, ntasks = 32, throttle = 200, extras = ["load python3"], time = "00:10:00")
-SlurmInterface("HPC_account_1", "CPU_partition", 1, 32, 200, "UQ_array", "", ["load python3"], "00:10:00")
+julia> slurm = SlurmInterface(Dict("account" => "HPC_account_1", "partition" => "CPU_partition"), extras = ["load python3"])
+SlurmInterface(Dict("account" => "HPC_account_1", "partition" => "CPU_partition"), 0, 0, ["load python3"])
 ```
 
 """
 Base.@kwdef struct SlurmInterface <: AbstractHPCScheduler
-    account::String
-    partition::String
-    nodes::Integer
-    ntasks::Integer
+    options::Dict{String,String}
     throttle::Integer = 0
     batchsize::Integer = 0
-    jobname::String = "UQ_array"
-    mempercpu::String = ""
     extras::Vector{String} = String[]
-    time::String = ""
+
+    function SlurmInterface(
+        options::Dict{String,String};
+        throttle::Integer=0,
+        batchsize::Integer=0,
+        extras::Vector{String}=String[],
+    )
+        if haskey(options, "array")
+            error("Do not pass array as an option.")
+        end
+
+        if !haskey(options, "account") || !haskey(options, "partition")
+            @warn "Omitting account or partition may cause your simulation to fail if your scheduler does not assign defaults."
+        end
+
+        return new(options, throttle, batchsize, extras)
+    end
 end
 
 function setup_hpc_jobs(si::SlurmInterface, m::ExternalModel, n::Integer, datetime::String)
@@ -97,21 +102,15 @@ function setup_slurm_array(
 
     open(fpath, "w") do file
         write(file, "#!/bin/bash -l\n")
-        write(file, "#SBATCH -A $(si.account)\n")
-        write(file, "#SBATCH -p $(si.partition)\n")
-        write(file, "#SBATCH -J $(si.jobname)\n")
-        write(file, "#SBATCH --nodes=$(si.nodes)\n")
-        write(file, "#SBATCH --ntasks=$(si.ntasks)\n")
+
+        for (k, v) in si.options
+            write(file, "#SBATCH --$k=$v\n")
+        end
+
         write(
             file, "#SBATCH --output=sample-%$(digits)a/UncertaintyQuantification-%a.out\n"
         )
         write(file, "#SBATCH --error=sample-%$(digits)a/UncertaintyQuantification-%a.err\n")
-        if !isempty(si.mempercpu)
-            write(file, "#SBATCH --mem-per-cpu=$(si.mempercpu)\n")
-        end
-        if !isempty(si.time)
-            write(file, "#SBATCH --time=$(si.time)\n")
-        end
         write(file, array_command)
         write(file, "\n\n\n")
         write(file, "#### EXTRAS ####\n")
