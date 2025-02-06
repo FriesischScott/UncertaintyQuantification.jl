c8104965d33b2e1a4a6c60f3007e3cf1ed12a8fb
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2025-02-06 16:25:46 +0100

    Update theme and config

--- docs/src/.vitepress/theme/index.ts
+++ docs/src/.vitepress/theme/index.ts
@@ -2,7 +2,11 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-
+import AsideTrustees from '../../components/AsideTrustees.vue'
+import VersionPicker from "../../components/VersionPicker.vue"
+import StarUs from '../../components/StarUs.vue'
+import AuthorBadge from '../../components/AuthorBadge.vue'
+import Authors from '../../components/Authors.vue'
 import { enhanceAppWithTabs } from 'vitepress-plugin-tabs/client'
 import './style.css'
 
@@ -10,10 +14,14 @@ export default {
   extends: DefaultTheme,
   Layout() {
     return h(DefaultTheme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'aside-ads-before': () => h(AsideTrustees),
+      'nav-bar-content-after': () => h(StarUs),
     })
   },
   enhanceApp({ app, router, siteData }) {
-    enhanceAppWithTabs(app)
+    enhanceAppWithTabs(app);
+    app.component('VersionPicker', VersionPicker);
+    app.component('AuthorBadge', AuthorBadge)
+    app.component('Authors', Authors)
   }
 } satisfies Theme
