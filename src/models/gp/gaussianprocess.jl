33b1782154a756cab292f5466eb234a9db31df8a
Cr0gan <felixmett@googlemail.com>
2025-02-10 20:24:42 +0100

    Started restructuring GaussianProcess constructors

--- src/models/gp/gaussianprocess.jl
+++ src/models/gp/gaussianprocess.jl
@@ -12,69 +12,65 @@ ResponseSurface([0.483333, -0.238636, 1.01894], :y, [:x], 2, Monomial{Commutativ
 ```
 """
 struct GaussianProcess <: UQModel
-    gp::AbstractGPs.AbstractGP
+    gp::AbstractGPs.FiniteGP
     input::Vector{Symbol}
-    output::Vector{Symbol}
-    inp_dim::Int
-    out_dim::Int
-    inp_transformer::AbstractInputTransformer # not sure if these should transform hyperparams as well
-    out_transformer::AbstractOutputTransformer
+    output::Symbol
+    input_transform::AbstractInputTransform # not sure if these should transform hyperparams as well
+    output_transform::AbstractOutputTransform
 end
 
-function GaussianProcess(    
-    gp::AbstractGPs.AbstractGP,
-    input::Symbol,
+""" ---------------------------------------------------------------------------------------------------- """
+# names = propertynames(data[:, Not(output)])
+## DATAFRAME INPUT OUTPUT STUFF
+function GaussianProcess(
+    gp::AbstractGPs.GP,
+    data::DataFrame,
     output::Symbol,
-    inp_dim::Int,
-    out_dim::Int,
-    inp_transformer::AbstractInputTransformer, # not sure if these should transform hyperparams as well
-    out_transformer::AbstractOutputTransformer, # leaving that for later
-)
-    GaussianProcess(
-        gp, [input], [output], 
-        inp_dim, out_dim, 
-        inp_transformer, out_transformer
-        )
+    input_transform::NoInputTransform,
+    output_transform::NoOutputTransform,
+    optimization::NoOptimization
+) # should we use keyword args?
+
+    return
 end
 
-function GaussianProcess(    
-    gp::AbstractGPs.AbstractGP,
-    input::Vector{Symbol},
+function GaussianProcess(
+    gp::AbstractGPs.GP,
+    data::DataFrame,
     output::Symbol,
-    inp_dim::Int,
-    out_dim::Int,
-    inp_transformer::AbstractInputTransformer, # not sure if these should transform hyperparams as well
-    out_transformer::AbstractOutputTransformer, # leaving that for later
-)
-    GaussianProcess(
-        gp, input, [output], 
-        inp_dim, out_dim, 
-        inp_transformer, out_transformer
-        )
+) # should we use keyword args?
+    return GaussianProcess(
+        gp,
+        data,
+        output,
+        NoInputTransform(),
+        NoOutputTransform(),
+        NoOptimization()
+    )
 end
 
-function GaussianProcess(    
-    gp::AbstractGPs.AbstractGP,
-    input::Symbol,
-    output::Vector{Symbol},
-    inp_dim::Int,
-    out_dim::Int,
-    inp_transformer::AbstractInputTransformer, # not sure if these should transform hyperparams as well
-    out_transformer::AbstractOutputTransformer, # leaving that for later
-)
-    GaussianProcess(
-        gp, [input], output, 
-        inp_dim, out_dim, 
-        inp_transformer, out_transformer
-        )
+function GaussianProcess(
+    gp::AbstractGPs.GP,
+    data::DataFrame,
+    output::Symbol,
+    input_transform::NoInputTransform,
+    output_transform::NoOutputTransform,
+    optimization::AbstractHyperparameterOptimization
+) # should we use keyword args?
+
+    return
 end
 
-""" """
-NoiseTypes = Union{
-    ParameterHandling.Positive, 
-    ParameterHandling.Bounded, 
-    ParameterHandling.Fixed
-    }
+function GaussianProcess(
+    gp::AbstractGPs.GP,
+    data::DataFrame,
+    output::Symbol;
+    input_transform::AbstractInputTransformer,
+    output_transform::AbstractOutputTransformer,
+    optimization::AbstractHyperparameterOptimization
+)
+    return
+end
 
 # Custom meanfunctions will break Zygote autodiff for multidimensional inputs
 # Create from DataFrame
@@ -110,76 +106,8 @@ function GaussianProcess(
         )
 end
 
-function GaussianProcess(
-    data::DataFrame,
-    inputs::Vector{Symbol},
-    outputs::Vector{Symbol},
-    build_gp::Function,
-    params::NamedTuple,
-    noise::NoiseTypes=positive(exp(-2.0)), # could support functions for noise as well...
-    normalize_inp::Bool=false,
-    normalize_out::Bool=false,
-    optimizer::Union{Optim.FirstOrderOptimizer, Nothing}=nothing
-)
-    (inp_dim, out_dim, 
-    inp_transformer, out_transformer, 
-    x, y) = _handle_gp_input(
-        data, inputs, outputs,
-        normalize_inp, normalize_out
-        )
-
-    θ = (;
-        mean_and_kernel = params,
-        noise = (;noise_params = noise)
-    )
-
-    gp = build_gp_posterior(build_gp, θ, x, y, optimizer)
-
-    return GaussianProcess(
-        gp, inputs, outputs, 
-        inp_dim, out_dim, 
-        inp_transformer, out_transformer
-        )
-end
-
-function GaussianProcess(
-    data::DataFrame,
-    inputs::Vector{Symbol},
-    outputs::Symbol,
-    build_gp::Function,
-    params::NamedTuple,
-    noise::NoiseTypes=positive(exp(-2.0)), # could support functions for noise as well...
-    normalize_inp::Bool=false,
-    normalize_out::Bool=false,
-    optimizer::Union{Optim.FirstOrderOptimizer, Nothing}=nothing
-)
-    return GaussianProcess(
-        data, inputs, [outputs],
-        build_gp, params, noise, 
-        normalize_inp, normalize_out, 
-        optimizer
-        )
-end
-
-function GaussianProcess(
-    data::DataFrame,
-    inputs::Symbol,
-    outputs::Vector{Symbol},
-    build_gp::Function,
-    params::NamedTuple,
-    noise::NoiseTypes=positive(exp(-2.0)), # could support functions for noise as well...
-    normalize_inp::Bool=false,
-    normalize_out::Bool=false,
-    optimizer::Union{Optim.FirstOrderOptimizer, Nothing}=nothing
-)
-    return GaussianProcess(
-        data, [inputs], outputs,
-        build_gp, params, noise, 
-        normalize_inp, normalize_out, 
-        optimizer
-        )
-end
-
+""" ---------------------------------------------------------------------------------------------------- """
+## UQMODEL STUFF
 """ experimental design """
 struct ExperimentalDesign # not sure about the name
     sim::AbstractMonteCarlo # could also allow doe
@@ -227,151 +155,6 @@ function GaussianProcess(
         )
 end
 
-# need to treat this differently because univariate in- and output
-function GaussianProcess(
-    inputs::UQInput,
-    model::UQModel,
-    outputs::Symbol,
-    exp_design::ExperimentalDesign,
-    build_gp::Function,
-    params::NamedTuple,
-    noise::NoiseTypes=positive(exp(-2.0)), # could support functions for noise as well...
-    normalize_inp::Bool=false,
-    normalize_out::Bool=false,
-    optimizer::Union{Optim.FirstOrderOptimizer, Nothing}=nothing
-)
-    return GaussianProcess(
-        inputs, [model], outputs, exp_design,
-        build_gp, params, noise,
-        normalize_inp, normalize_out, 
-        optimizer
-        )
-end
-
-# All these cases dispatch to the same method
-function GaussianProcess(
-    inputs::Vector{<:UQInput},
-    model::Vector{<:UQModel},
-    outputs::Vector{Symbol},
-    exp_design::ExperimentalDesign,
-    build_gp::Function,
-    params::NamedTuple,
-    noise::NoiseTypes=positive(exp(-2.0)), # could support functions for noise as well...
-    normalize_inp::Bool=false,
-    normalize_out::Bool=false,
-    optimizer::Union{Optim.FirstOrderOptimizer, Nothing}=nothing
-)
-    data = sample(inputs, exp_design.sim) # need to be able to pass experimental design
-    evaluate!(model, data)
-
-    return GaussianProcess(
-        data, inputs, outputs,
-        build_gp, params, noise,
-        normalize_inp, normalize_out, 
-        optimizer
-        )
-end
-
-function GaussianProcess(
-    inputs::Vector{<:UQInput},
-    model::UQModel,
-    outputs::Vector{Symbol},
-    exp_design::ExperimentalDesign,
-    build_gp::Function,
-    params::NamedTuple,
-    noise::NoiseTypes=positive(exp(-2.0)), # could support functions for noise as well...
-    normalize_inp::Bool=false,
-    normalize_out::Bool=false,
-    optimizer::Union{Optim.FirstOrderOptimizer, Nothing}=nothing
-)
-    return GaussianProcess(
-        inputs, [model], outputs, exp_design,
-        build_gp, params, noise,
-        normalize_inp, normalize_out, 
-        optimizer
-        )
-end
-
-function GaussianProcess(
-    inputs::Vector{<:UQInput},
-    model::UQModel,
-    outputs::Symbol,
-    exp_design::ExperimentalDesign,
-    build_gp::Function,
-    params::NamedTuple,
-    noise::NoiseTypes=positive(exp(-2.0)), # could support functions for noise as well...
-    normalize_inp::Bool=false,
-    normalize_out::Bool=false,
-    optimizer::Union{Optim.FirstOrderOptimizer, Nothing}=nothing
-)
-    return GaussianProcess(
-        inputs, [model], [outputs], exp_design,
-        build_gp, params, noise,
-        normalize_inp, normalize_out, 
-        optimizer
-        )
-end
-
-function GaussianProcess(
-    inputs::UQInput,
-    model::Vector{<:UQModel},
-    outputs::Vector{Symbol},
-    exp_design::ExperimentalDesign,
-    build_gp::Function,
-    params::NamedTuple,
-    noise::NoiseTypes=positive(exp(-2.0)), # could support functions for noise as well...
-    normalize_inp::Bool=false,
-    normalize_out::Bool=false,
-    optimizer::Union{Optim.FirstOrderOptimizer, Nothing}=nothing
-)
-    return GaussianProcess(
-        [inputs], model, outputs, exp_design,
-        build_gp, params, noise,
-        normalize_inp, normalize_out, 
-        optimizer
-        )
-end
-
-function GaussianProcess(
-    inputs::UQInput,
-    model::UQModel,
-    outputs::Vector{Symbol},
-    exp_design::ExperimentalDesign,
-    build_gp::Function,
-    params::NamedTuple,
-    noise::NoiseTypes=positive(exp(-2.0)), # could support functions for noise as well...
-    normalize_inp::Bool=false,
-    normalize_out::Bool=false,
-    optimizer::Union{Optim.FirstOrderOptimizer, Nothing}=nothing
-)
-    return GaussianProcess(
-        [inputs], [model], outputs, exp_design,
-        build_gp, params, noise,
-        normalize_inp, normalize_out, 
-        optimizer
-        )
-end
-
-function GaussianProcess(
-    inputs::Vector{<:UQInput},
-    model::Vector{<:UQModel},
-    outputs::Symbol,
-    exp_design::ExperimentalDesign,
-    build_gp::Function,
-    params::NamedTuple,
-    noise::NoiseTypes=positive(exp(-2.0)), # could support functions for noise as well...
-    normalize_inp::Bool=false,
-    normalize_out::Bool=false,
-    optimizer::Union{Optim.FirstOrderOptimizer, Nothing}=nothing
-)
-    return GaussianProcess(
-        inputs, model, [outputs], exp_design,
-        build_gp, params, noise,
-        normalize_inp, normalize_out, 
-        optimizer
-        )
-end
-
 # what should this calculate? Calculates mean for now
 function evaluate!(gp::GaussianProcess, df::DataFrame) # this now gives mean and variance at input
     x = gp.inp_transformer(df)
@@ -391,47 +174,6 @@ function evaluate!(gp::GaussianProcess, df::DataFrame) # this now gives mean and
 
     insertcols!(df, (gp.output .=> eachcol(y))...)
     return nothing
-end
-
-""" maximize_logml(logml, θ, x, y, build_gp; optimizer=optimizer) """
-function logml(θ, x, y, build_gp)
-    gp = build_gp(ParameterHandling.value(θ.mean_and_kernel))
-    f = gp(
-        x, 
-        only(ParameterHandling.value(θ.noise))^2 # same as in GaussianProcess...
-        )
-    return -logpdf(f, y)
-end
-
-function maximize_logml(logml, θ, x, y, build_gp; optimizer, maxiter=1_000)
-    options = Optim.Options(; iterations=maxiter, show_trace=true)
-
-    θ_flat, unflatten = ParameterHandling.value_flatten(θ)
-
-    ## https://julianlsolvers.github.io/Optim.jl/stable/#user/tipsandtricks/#avoid-repeating-computations
-    function fg!(F, G, θᵢ)
-        if F !== nothing && G !== nothing
-            val, grad = Zygote.withgradient(
-                θᵢ -> logml(unflatten(θᵢ), x, y, build_gp), 
-                θᵢ
-                )
-            G .= only(grad)
-            return val
-        elseif G !== nothing
-            grad = Zygote.gradient(
-                θᵢ -> logml(unflatten(θᵢ), x, y, build_gp), 
-                θᵢ
-                )
-            G .= only(grad)
-            return nothing
-        elseif F !== nothing
-            return logml(unflatten(θᵢ), x, y, build_gp)
-        end
-    end
-
-    result = optimize(Optim.only_fg!(fg!), θ_flat, optimizer, options; inplace=false)
-
-    return unflatten(result.minimizer), result
 end
 
 function build_gp_posterior(
