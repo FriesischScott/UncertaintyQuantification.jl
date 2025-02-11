95ee9c9f7f8beed7a8ff4f5683e8c6778d9b9004
Cr0gan <felixmett@googlemail.com>
2025-02-11 19:08:48 +0100

    Simple demo of current state

--- demo/metamodels/gaussianprocess.jl
+++ demo/metamodels/gaussianprocess.jl
@@ -1,82 +1,54 @@
-using UncertaintyQuantification, Plots
-using AbstractGPs # not sure if to reexport
-using ParameterHandling # not sure if to reexport
-using Optim
+using UncertaintyQuantification
+using AbstractGPs
 using Random
 
-Random.seed!(20140430)
-n = 10
-input_symbol = :x
-output_symbol = :y
-
-# Input_symbol for passing a UQModel to GaussianProcess
-x = RandomVariable(Uniform(0, 2π), input_symbol)
-y = Model(
-    df ->
-        (sin.(df.x) + 0.05*randn(length(df.x))),
-        output_symbol,
+# Setup Himmelblau example
+x = RandomVariable.(Uniform(-5, 5), [:x1, :x2])
+himmelblau = Model(
+    df -> (df.x1 .^ 2 .+ df.x2 .- 11) .^ 2 .+ (df.x1 .+ df.x2 .^ 2 .- 7) .^ 2, :y
 )
-exp_design = ExperimentalDesign(MonteCarlo(n))
+design = FullFactorial([8, 8])
+training_data = sample(x, design)
+evaluate!(himmelblau, training_data)
 
-# Input_symbol for passing a DataFrame to GaussianProcess
-df = sample(x, n)
-evaluate!(y, df)
+# This will be used for proper initial guesses for the parameters of the GP
+mean_data = mean(training_data[!, :y])
+std_data = std(training_data[!, :y])
 
-# Define how the GP is build
-function build_gp(θ::NamedTuple)
-    k1 = (θ.SE.σ)^2 * with_lengthscale(SqExponentialKernel(), θ.SE.ℓ)
-    k2 = (θ.RQ.σ)^2 * with_lengthscale(RationalQuadraticKernel(; α=θ.RQ.α), θ.RQ.ℓ)
-    return GP(k1+k2)
+# Setup the GP
+# Note: If we do not initialize the parameters here properly the optimization will fail. Standardization should help with that.
+σ² = 1e-5
+kernel = SqExponentialKernel() ∘ ARDTransform([1.0, 1.0])
+gp = with_gaussian_noise(GP(0.0, kernel), σ²)
+
+optimizer = MLE()
+# TODO: StandardizeInput breaks currently due to -Inf and Inf values from to_standard_normal_space!()
+# TODO: Optimization is extremely unstable
+# TODO: Not all kernels have a extract_parameters and apply_parameters function 
+gpr = GaussianProcess(
+    gp, x, himmelblau, :y, design, StandardizeOutput(), MLE()
+)
+
+test_data = sample(x, 1000)
+evaluate!(gpr, test_data)
+
+p_data = test_data[:, [:x1, :x2]]
+evaluate!(himmelblau, p_data)
+
+mse = mean((p_data.y .- test_data.y) .^ 2)
+println("MSE is:  $mse")
+
+using Plots
+using DataFrames
+a = range(-5, 5; length=1000) 
+b = range(5, -5; length=1000)
+himmelblau_f(x1, x2) = (x1^2 + x2 - 11)^2 + (x1 + x2^2 - 7)^2
+function gpr_f(x, y)
+    df = DataFrame(x1 = x, x2 = y)
+    evaluate!(gpr, df)
+    return only(df[:, :y])
 end
 
-# gp parameters
-θ = (;
-    SE = (;
-        σ = positive(1.),
-        ℓ = positive(1.)
-    ),
-    RQ = (;
-        σ = positive(1.),
-        ℓ = positive(1.),
-        α = positive(exp(-1.0))
-    )
-)
-
-# noise
-noise = fixed(exp(-2.)) 
-
-# Fit GP to data from experimental design
-gp_from_model = GaussianProcess(
-    x,
-    y,
-    :y,
-    exp_design,
-    build_gp,
-    θ,
-    noise
-)
-
-# Optimize hyperparameters (There should be a method that allows to do this on a already fitted gp instance)
-opt_gp_from_model = GaussianProcess(
-    x,
-    y,
-    :y,
-    exp_design,
-    build_gp,
-    θ,
-    noise,
-    false,
-    false,
-    LBFGS()
-)
-
-function plotdata()
-    plot(; xlabel="x", ylabel="y", legend=:bottomright)
-    return scatter!(df[:, 1], df[:, 2]; label="training data", ms=2, markerstrokewidth=0)
-end
-
-plot_gp!(f; label) = plot!(f(sort!(sample(x, 100)[:, 1])); ribbon_scale=2, linewidth=1, label)
-
-plotdata()
-plot_gp!(gp_from_model.gp; label="posterior f(⋅)")
-plot_gp!(opt_gp_from_model.gp; label="posterior f(⋅) optimized")
+s1 = surface(a, b, himmelblau_f; plot_title="Himmelblau's function")
+s2 = surface(a, b, gpr_f; plot_title="Gaussian process regression")
+plot(s1, s2, layout = (1, 2), legend = false)
