049c20bf3b6814b956baf02e88890d879f56b159
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2025-02-20 11:32:25 +0100

    Update docstring

--- src/inputs/empiricaldistribution.jl
+++ src/inputs/empiricaldistribution.jl
@@ -1,9 +1,10 @@
 """
-    EmpiricalDistribution(x::Vector{<:Real})
+    EmpiricalDistribution(x::Vector{<:Real}, n::Integer=10000)
 
     Creates an empirical distribution from the data given in `x` using kernel density estimation.
     The kernel used is Gaussian and the bandwidth is obtained through the Sheather-Jones method.
     The support is inferred from the kde using numerical root finding.
+    The `cdf` and `quantile` functions are linearly interpolated using `n` data points.
 """
 struct EmpiricalDistribution <: ContinuousUnivariateDistribution
     data::Vector{<:Real}
