0eebc63b006e17d0e98e2f4bf5499c1147eca4e9
Jasper Behrensdorf <behrensdorf@irz.uni-hannover.de>
2025-02-25 13:55:20 +0100

    Improve sampling outside beta-sphere
    Co-authored-by: Matteo Broggi <broggi@irz.uni-hannover.de>

--- src/simulations/radialbasedimportancesampling.jl
+++ src/simulations/radialbasedimportancesampling.jl
@@ -27,32 +27,21 @@ function sample(inputs::Vector{<:UQInput}, sim::RadialBasedImportanceSampling)
     return samples
 end
 
-#=
-L. E. Blumenson, ‘A Derivation of n-Dimensional Spherical Coordinates’, The American Mathematical Monthly, vol. 67, no. 1, pp. 63–66, 1960, doi: 10.2307/2308932.
-=#
 function _samples_outside_beta_sphere(β::Real, k::Integer, n::Integer)
+
+    # sample chi square distributed radius > β
     r = sqrt.(rand(truncated(Chisq(k), β^2, Inf), n))
-    if k == 1
-        return r
-    elseif k == 2
-        φ = rand(Uniform(0, 2π), n)
 
-        return [r .* cos.(φ) r .* sin.(φ)]
-    else
-        φ = rand(Uniform(0, π), (n, k - 2))
+    # standard normal samples
+    ϕ = rand(Normal(), (n, k))
+    # normalize each row to create samples on the surface of an n-sphere with radius 1.0
+    ϕ = ϕ ./ norm.(eachrow(ϕ))
 
-        θ = rand(Uniform(0, 2π), n)
+    # scale samples with the radius to > β
+    x = ϕ .* r
 
-        x = r .* cos.(φ[:, 1])
+    # error if samples are generated inside the sphere
+    @assert all(norm.(eachrow(x)) .> β)
 
-        for j in 2:(k - 2)
-            x = hcat(x, r .* cos.(φ[:, j]) .* prod(sin.(φ[:, 1:(j - 1)]); dims=2))
-        end
-
-        x = hcat(x, r .* sin.(θ) .* prod(sin.(φ[:, 1:(k - 2)]); dims=2))
-
-        x = hcat(x, r .* cos.(θ) .* prod(sin.(φ[:, 1:(k - 2)]); dims=2))
-
-        return x
-    end
+    return x
 end
