740603600306c539ebfac42f46f3df5c9ffee9af
Felix Mett <119941890+Cr0gan@users.noreply.github.com>
2025-08-19 17:25:05 +0200

    Refactoring of type II maximum likelihood estimation

--- src/models/gp/hyperparametertuning.jl
+++ src/models/gp/hyperparametertuning.jl
@@ -1,45 +1,49 @@
+using DifferentiationInterface
+
 abstract type AbstractHyperparameterOptimization end
 
 struct NoOptimization <: AbstractHyperparameterOptimization end
 
-function optimize_hyperparameters(gp::Union{AbstractGPs.GP, NoisyGP}, x, y, opt::NoOptimization) #!TYPES
-    return gp
-end
-
-struct MLE <: AbstractHyperparameterOptimization
+struct MaximumLikelihoodEstimation <: AbstractHyperparameterOptimization
     optimizer::Optim.FirstOrderOptimizer
     options::Optim.Options
 end
 
-MLE() = MLE(Optim.LBFGS(), Optim.Options(; iterations=1000, show_trace=true))
+MaximumLikelihoodEstimation() = MaximumLikelihoodEstimation(
+    Optim.LBFGS(),
+    Optim.Options(; iterations=1000, show_trace=false)
+)
 
-objective(f::Union{AbstractGPs.GP, NoisyGP}, x, y, mle::MLE) = -logpdf(f(x), y)
+function optimize_hyperparameters(
+    gp::Union{AbstractGPs.GP, NoisyGP}, 
+    x, 
+    y, 
+    opt::NoOptimization
+)
+    return gp
+end
 
-function optimize_hyperparameters(gp::Union{AbstractGPs.GP, NoisyGP}, x, y, mle::MLE) #!TYPES
+objective(
+    f::Union{AbstractGPs.GP, NoisyGP}, 
+    x, 
+    y, 
+    mle::MaximumLikelihoodEstimation
+) = -logpdf(f(x), y)
+
+function optimize_hyperparameters(
+    gp::Union{AbstractGPs.GP, NoisyGP}, 
+    x, 
+    y, 
+    mle::MaximumLikelihoodEstimation
+) #!TYPES
     model, θ₀ = parameterize(gp)
-    flatparams, unflatten = ParameterHandling.flatten(θ₀)
+    θ₀_flat, unflatten = ParameterHandling.flatten(θ₀)
 
-    ## https://julianlsolvers.github.io/Optim.jl/stable/#user/tipsandtricks/#avoid-repeating-computations
-    function fg!(F, G, θ)
-        if F !== nothing && G !== nothing
-            val, grad = Zygote.withgradient(
-                θ -> objective(model(unflatten(θ)), x, y, mle), 
-                θ
-                )
-            G .= only(grad)
-            return val
-        elseif G !== nothing
-            grad = Zygote.gradient(
-                θ -> objective(model(unflatten(θ)), x, y, mle), 
-                θ
-                )
-            G .= only(grad)
-            return nothing
-        elseif F !== nothing
-            return objective(model(unflatten(θ)), x, y, mle)
-        end
-    end
-
-    result = optimize(Optim.only_fg!(fg!), flatparams, mle.optimizer, mle.options; inplace=false)
+    result = optimize(
+        θ -> objective(model(unflatten(θ)), x, y, mle), 
+        θ₀_flat, 
+        mle.optimizer, mle.options; 
+        autodiff=AutoZygote()
+        )
     return model(unflatten(result.minimizer))
 end
