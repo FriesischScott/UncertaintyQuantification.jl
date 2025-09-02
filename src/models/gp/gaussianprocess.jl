bc773310aede317f1029625e22ba017899e84a15
Cr0gan <felixmett@googlemail.com>
2025-09-02 17:57:59 +0200

    Change to single DataTransforms struct to handle transformations

--- src/models/gp/gaussianprocess.jl
+++ src/models/gp/gaussianprocess.jl
@@ -2,215 +2,85 @@ struct GaussianProcess <: UQModel
     gp::AbstractGPs.PosteriorGP
     input::Union{Symbol, Vector{Symbol}}
     output::Symbol
-    input_transform::Function
-    output_transform::Function
+    data_transforms::DataTransforms
 end
 
-# Build from Dataframe
+# ---------------- Build from DataFrame ----------------
 function GaussianProcess(
     gp::Union{AbstractGPs.GP, NoisyGP},
     data::DataFrame,
-    output::Symbol,
-    instandard::StandardizeInput,
-    outstandard::StandardizeOutput,
-    optimization::AbstractHyperparameterOptimization
-) # should we use keyword args?
-    input = propertynames(data[:, Not(output)])
-    datatransformer = DataTransform(
-        data, instandard, input, outstandard, output
+    output::Symbol;
+    input_transform::AbstractInputTransform=ZScoreInputTransform(),
+    output_transform::AbstractOutputTransform=ZScoreOutputTransform(),
+    optimization::AbstractHyperparameterOptimization=MaximumLikelihoodEstimation()
+) 
+    input = propertynames(data[:, Not(output)]) # Is this always the case?
+
+    # build in- and output transforms
+    dts = DataTransforms(
+        data, input, output, 
+        input_transform, 
+        output_transform
     )
-    x = datatransformer.input(data)
-    y = datatransformer.output(data)
+
+    # transform data
+    x = dts.fᵢ(data)
+    y = dts.fₒ(data)
+
+    # build posterior gp
     optimized_gp = optimize_hyperparameters(gp, x, y, optimization)
     posterior_gp = posterior(optimized_gp(x), y)
     return GaussianProcess(
         posterior_gp,
         input,
         output,
-        datatransformer
+        dts
     )
 end
 
-function GaussianProcess(
-    gp::AbstractGPs.GP,
-    data::DataFrame,
-    output::Symbol,
-    instandard::StandardizeInput,
-    optimization::AbstractHyperparameterOptimization
-) # should we use keyword args?
-    return GaussianProcess(
-        gp,
-        data,
-        output,
-        instandard,
-        StandardizeOutput(false),
-        optimization
-    )
-end
-
-function GaussianProcess(
-    gp::AbstractGPs.GP,
-    data::DataFrame,
-    output::Symbol,
-    outstandard::StandardizeOutput,
-    optimization::AbstractHyperparameterOptimization
-) # should we use keyword args?
-    return GaussianProcess(
-        gp,
-        data,
-        output,
-        StandardizeInput(false),
-        outstandard,
-        optimization
-    )
-end
-
-function GaussianProcess(
-    gp::AbstractGPs.GP,
-    data::DataFrame,
-    output::Symbol,
-    optimization::AbstractHyperparameterOptimization
-) # should we use keyword args?
-    return GaussianProcess(
-        gp,
-        data,
-        output,
-        StandardizeInput(false),
-        StandardizeOutput(false),
-        optimization
-    )
-end
-
-function GaussianProcess(
-    gp::AbstractGPs.GP,
-    data::DataFrame,
-    output::Symbol
-) # should we use keyword args?
-    return GaussianProcess(
-        gp,
-        data,
-        output,
-        StandardizeInput(false),
-        StandardizeOutput(false),
-        NoOptimization()
-    )
-end
-
-# Build with UQmodel
+# ---------------- Build from UQModel ----------------
 function GaussianProcess(
     gp::Union{AbstractGPs.GP, NoisyGP},
     input::Union{UQInput, Vector{<:UQInput}},
     model::Union{UQModel, Vector{<:UQModel}},
     output::Symbol,
-    experimentaldesign::Union{AbstractMonteCarlo, AbstractDesignOfExperiments},
-    instandard::StandardizeInput,
-    outstandard::StandardizeOutput,
-    optimization::AbstractHyperparameterOptimization
+    experimentaldesign::Union{AbstractMonteCarlo, AbstractDesignOfExperiments};
+    input_transform::AbstractInputTransform=ZScoreInputTransform(),
+    output_transform::AbstractOutputTransform=ZScoreOutputTransform(),
+    optimization::AbstractHyperparameterOptimization=MaximumLikelihoodEstimation()
 )
-    data = sample(input, experimentaldesign) # need to be able to pass experimental design
+    # build DataFrame
+    data = sample(input, experimentaldesign)
     evaluate!(model, data)
 
-    datatransformer = DataTransform(
-        data, instandard, input, outstandard, output
+    # build in- and output transforms
+    dts = DataTransforms(
+        data, input, output, 
+        input_transform, 
+        output_transform
     )
-    x = datatransformer.input(data)
-    y = datatransformer.output(data)
+
+    # transform data
+    x = dts.fᵢ(data)
+    y = dts.fₒ(data)
+
+    # build posterior gp
     optimized_gp = optimize_hyperparameters(gp, x, y, optimization)
     posterior_gp = posterior(optimized_gp(x), y)
+
     return GaussianProcess(
         posterior_gp,
         names(input),
         output,
-        datatransformer
-    )
-end
-
-function GaussianProcess(
-    gp::Union{AbstractGPs.GP, NoisyGP},
-    input::Union{UQInput, Vector{<:UQInput}},
-    model::Union{UQModel, Vector{<:UQModel}},
-    output::Symbol,
-    experimentaldesign::Union{AbstractMonteCarlo, AbstractDesignOfExperiments},
-    instandard::StandardizeInput,
-    optimization::AbstractHyperparameterOptimization
-)
-    return GaussianProcess(
-        gp,
-        input,
-        model, 
-        output,
-        experimentaldesign,
-        instandard,
-        StandardizeOutput(false),
-        optimization
-    )
-end
-
-function GaussianProcess(
-    gp::Union{AbstractGPs.GP, NoisyGP},
-    input::Union{UQInput, Vector{<:UQInput}},
-    model::Union{UQModel, Vector{<:UQModel}},
-    output::Symbol,
-    experimentaldesign::Union{AbstractMonteCarlo, AbstractDesignOfExperiments},
-    outstandard::StandardizeOutput,
-    optimization::AbstractHyperparameterOptimization
-)
-    return GaussianProcess(
-        gp,
-        input,
-        model, 
-        output,
-        experimentaldesign,
-        StandardizeInput(false),
-        outstandard,
-        optimization
-    )
-end
-
-function GaussianProcess(
-    gp::Union{AbstractGPs.GP, NoisyGP},
-    input::Union{UQInput, Vector{<:UQInput}},
-    model::Union{UQModel, Vector{<:UQModel}},
-    output::Symbol,
-    experimentaldesign::Union{AbstractMonteCarlo, AbstractDesignOfExperiments},
-    optimization::AbstractHyperparameterOptimization
-)
-    return GaussianProcess(
-        gp,
-        input,
-        model, 
-        output,
-        experimentaldesign,
-        StandardizeInput(false),
-        StandardizeOutput(false),
-        optimization
-    )
-end
-
-function GaussianProcess(
-    gp::Union{AbstractGPs.GP, NoisyGP},
-    input::Union{UQInput, Vector{<:UQInput}},
-    model::Union{UQModel, Vector{<:UQModel}},
-    output::Symbol,
-    experimentaldesign::Union{AbstractMonteCarlo, AbstractDesignOfExperiments}
-)
-    return GaussianProcess(
-        gp,
-        input,
-        model, 
-        output,
-        experimentaldesign,
-        StandardizeInput(false),
-        StandardizeOutput(false),
-        NoOptimization()
+        dts
     )
 end
 
 # what should this calculate? Calculates only mean for now
 function evaluate!(gp::GaussianProcess, data::DataFrame)
-    x = gp.datatransformer.input(data)
+    x = gp.data_transforms.fᵢ(data)
     y = mean(gp.gp(x))
 
-    data[!, gp.output] = inverse_transform(y, gp.datatransformer.output)
+    data[!, gp.output] = gp.data_transforms.fₒ⁻¹(y) # applying inverse transform to output
     return nothing
 end
