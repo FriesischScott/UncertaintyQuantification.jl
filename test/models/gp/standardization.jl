8e9f339d600a34beb0bc233b49a940069eda9da2
Felix Mett <119941890+Cr0gan@users.noreply.github.com>
2025-09-03 20:33:49 +0200

    Preliminary tests

--- test/models/gp/standardization.jl
+++ test/models/gp/standardization.jl
@@ -9,34 +9,183 @@
     df_single_in[!, output] = rand(N)
 
     df_multi_in = sample(multi_input, N)
-    df_multi_in[!, output] = rand(N)
+    df_multi_in[!, output] = df_single_in[!, output]
 
     single_input_names = propertynames(df_single_in[:, Not(output)])
     multi_input_names = propertynames(df_multi_in[:, Not(output)])
 
     @testset "IdentityTransform" begin
-        dts_single = DataStandardizer(
+        # 1D input
+        dts_single = UncertaintyQuantification.DataStandardizer(
             df_single_in, single_input_names, output, 
-            InputTransform(IdentityTransform()), 
-            OutputTransform(IdentityTransform())
+            UncertaintyQuantification.InputTransform(
+                UncertaintyQuantification.IdentityTransform()
+                ), 
+            UncertaintyQuantification.OutputTransform(
+                UncertaintyQuantification.IdentityTransform()
+                )
         )
-
-        dts_multi = DataStandardizer(
-            df_multi_in, multi_input_names, output, 
-            InputTransform(IdentityTransform()), 
-            OutputTransform(IdentityTransform())
-        )
-
         single_in_transformed = dts_single.fᵢ(df_single_in)
-        multi_in_transformed = dts_single.fᵢ(df_single_in)
+        
+        @test isa(single_in_transformed, Vector)
+        @test all(single_in_transformed .== df_single_in[:, only(single_input_names)])
+
+        # ND input
+        dts_multi = UncertaintyQuantification.DataStandardizer(
+            df_multi_in, multi_input_names, output, 
+            UncertaintyQuantification.InputTransform(
+                UncertaintyQuantification.IdentityTransform()
+                ), 
+            UncertaintyQuantification.OutputTransform(
+                UncertaintyQuantification.IdentityTransform()
+                )
+        )
+        multi_in_transformed = dts_multi.fᵢ(df_multi_in)
+
+        @test isa(multi_in_transformed, RowVecs)
+
+        RowVecsMatrix = mapreduce(rv -> rv', vcat, multi_in_transformed)
+        @test all(RowVecsMatrix .== Matrix(df_multi_in[:, multi_input_names]))
+
+        # Output
+        out_transformed = dts_single.fₒ(df_single_in)
+
+        # single input and multi input related output transforms should do the same thing
+        @test all(out_transformed .== dts_multi.fₒ(df_multi_in))
+
+        # Mean and latent function samples just get identity transformed
+        @test all(df_single_in[!, output] .== dts_single.fₒ⁻¹(out_transformed))
+        @test all(df_multi_in[!, output] .== dts_multi.fₒ⁻¹(out_transformed))
+
+        # Variance also just get identity transformed
+        @test all(df_single_in[!, output] .== dts_single.var_fₒ⁻¹(out_transformed))
+        @test all(df_multi_in[!, output] .== dts_multi.var_fₒ⁻¹(out_transformed))
     end
 
     @testset "ZScoreTransform" begin
+        # 1D input
+        dts_single = UncertaintyQuantification.DataStandardizer(
+            df_single_in, single_input_names, output, 
+            UncertaintyQuantification.InputTransform(
+                UncertaintyQuantification.ZScoreTransform()
+                ), 
+            UncertaintyQuantification.OutputTransform(
+                UncertaintyQuantification.ZScoreTransform()
+                )
+        )
+        single_in_transformed = dts_single.fᵢ(df_single_in)
         
+        @test isa(single_in_transformed, Vector)
+
+        μ = mean(df_single_in[:, only(single_input_names)])
+        σ = std(df_single_in[:, only(single_input_names)])
+        manually_scaled = (df_single_in[:, only(single_input_names)] .- μ) ./ σ
+        @test all(single_in_transformed .≈ manually_scaled)
+
+        # ND input
+        dts_multi = UncertaintyQuantification.DataStandardizer(
+            df_multi_in, multi_input_names, output, 
+            UncertaintyQuantification.InputTransform(
+                UncertaintyQuantification.ZScoreTransform()
+                ), 
+            UncertaintyQuantification.OutputTransform(
+                UncertaintyQuantification.ZScoreTransform()
+                )
+        )
+        multi_in_transformed = dts_multi.fᵢ(df_multi_in)
+
+        @test isa(multi_in_transformed, RowVecs)
+
+        RowVecsMatrix = mapreduce(rv -> rv', vcat, multi_in_transformed)
+        μ = mean(Matrix(df_multi_in[:, multi_input_names]), dims=1)
+        σ = std(Matrix(df_multi_in[:, multi_input_names]), dims=1)
+
+        manually_scaled = (Matrix(df_multi_in[:, multi_input_names]) .- μ) ./ σ
+        @test all(RowVecsMatrix .≈ manually_scaled)
+
+        # Output
+        out_transformed = dts_single.fₒ(df_single_in)
+
+        # single input and multi input related output transforms should do the same thing
+        @test all(out_transformed .== dts_multi.fₒ(df_multi_in))
+
+        # Mean and latent function samples get rescaled and shifted
+        @test all(df_single_in[!, output] .≈ dts_single.fₒ⁻¹(out_transformed))
+        @test all(df_multi_in[!, output] .≈ dts_multi.fₒ⁻¹(out_transformed))
+
+        # Variance gets multiplied by squared standard deviation used in ZScoreTransform
+        # Note: This usually gets applied to the GP posterior variance, here
+        # we just scale out_transformed back to verify it does the right thing
+        σ = std(df_single_in[:, output])
+
+        @test all(out_transformed * σ^2 .≈ dts_single.var_fₒ⁻¹(out_transformed))
+        @test all(out_transformed * σ^2 .≈ dts_multi.var_fₒ⁻¹(out_transformed))
     end
 
     @testset "UnitRangeTransform" begin
+        # 1D input
+        dts_single = UncertaintyQuantification.DataStandardizer(
+            df_single_in, single_input_names, output, 
+            UncertaintyQuantification.InputTransform(
+                UncertaintyQuantification.UnitRangeTransform()
+                ), 
+            UncertaintyQuantification.OutputTransform(
+                UncertaintyQuantification.UnitRangeTransform()
+                )
+        )
+        single_in_transformed = dts_single.fᵢ(df_single_in)
         
+        @test isa(single_in_transformed, Vector)
+
+        tmin, tmax = extrema(df_single_in[:, only(single_input_names)])
+        scale = 1 / (tmax - tmin)
+        manually_scaled = (df_single_in[:, only(single_input_names)] .- tmin) * scale
+        @test all(single_in_transformed .≈ manually_scaled)
+
+        # ND input
+        dts_multi = UncertaintyQuantification.DataStandardizer(
+            df_multi_in, multi_input_names, output, 
+            UncertaintyQuantification.InputTransform(
+                UncertaintyQuantification.UnitRangeTransform()
+                ), 
+            UncertaintyQuantification.OutputTransform(
+                UncertaintyQuantification.UnitRangeTransform()
+                )
+        )
+        multi_in_transformed = dts_multi.fᵢ(df_multi_in)
+
+        @test isa(multi_in_transformed, RowVecs)
+
+        RowVecsMatrix = mapreduce(rv -> rv', vcat, multi_in_transformed)
+        mins_maxs = extrema(df_single_in[:, only(single_input_names)], dims=1)
+        tmin = map(t -> t[1], mins_maxs[1, :])
+        scale = map(t -> 1 / (t[2] - t[1]), mins_maxs[1, :])
+
+        manually_scaled = (Matrix(df_multi_in[:, multi_input_names]) .- tmin) .* scale
+        print(RowVecsMatrix)
+        print(manually_scaled)
+        @test all(RowVecsMatrix .≈ manually_scaled)
+
+        # Output
+        out_transformed = dts_single.fₒ(df_single_in)
+
+        # single input and multi input related output transforms should do the same thing
+        @test all(out_transformed .== dts_multi.fₒ(df_multi_in))
+
+        # Mean and latent function samples get rescaled and shifted
+        @test all(df_single_in[!, output] .≈ dts_single.fₒ⁻¹(out_transformed))
+        @test all(df_multi_in[!, output] .≈ dts_multi.fₒ⁻¹(out_transformed))
+
+        # Variance gets multiplied by squared scale used in UnitRangeTransform
+        # Note: This usually gets applied to the GP posterior variance, here
+        # we just scale out_transformed back to verify it does the right thing
+        tmin, tmax = extrema(df_single_in[:, output])
+        scale = 1 / (tmax - tmin)
+
+        print(out_transformed * (1/scale)^2)
+        print(dts_single.var_fₒ⁻¹(out_transformed))
+        @test all(out_transformed * (1/scale)^2 .≈ dts_single.var_fₒ⁻¹(out_transformed))
+        @test all(out_transformed * (1/scale)^2 .≈ dts_multi.var_fₒ⁻¹(out_transformed))
     end
 
     @testset "StandardNormalTransform" begin
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,4 @@
+using AbstractGPs
 using DataFrames
 using Distributed
 using HCubature
@@ -9,59 +10,60 @@ using StatsBase: fit, Histogram, corkendall
 using Test
 using UncertaintyQuantification
 
-include("inputs/empiricaldistribution.jl")
-include("dynamics/psd.jl")
-include("inputs/parameter.jl")
-include("inputs/jointdistribution.jl")
-include("inputs/imprecise/interval.jl")
-include("inputs/imprecise/p-box.jl")
-include("inputs/randomvariables/randomvariable.jl")
-include("inputs/randomvariables/distributionparameters.jl")
-include("inputs/jointdistribution.jl");
-include("inputs/inputs.jl")
-include("inputs/copulas/gaussian.jl")
-include("inputs/stochasticprocesses/spectralrepresentation.jl")
-include("inputs/stochasticprocesses/models.jl")
+# include("inputs/empiricaldistribution.jl")
+# include("dynamics/psd.jl")
+# include("inputs/parameter.jl")
+# include("inputs/jointdistribution.jl")
+# include("inputs/imprecise/interval.jl")
+# include("inputs/imprecise/p-box.jl")
+# include("inputs/randomvariables/randomvariable.jl")
+# include("inputs/randomvariables/distributionparameters.jl")
+# include("inputs/jointdistribution.jl");
+# include("inputs/inputs.jl")
+# include("inputs/copulas/gaussian.jl")
+# include("inputs/stochasticprocesses/spectralrepresentation.jl")
+# include("inputs/stochasticprocesses/models.jl")
 
-include("models/external/solvers.jl")
-include("models/external/externalmodel.jl")
-include("models/model.jl")
-include("models/polyharmonicspline.jl")
-include("models/pce/pcebases.jl")
-include("models/pce/polynomialchaosexpansion.jl")
-include("models/responsesurface.jl")
-include("models/imprecise/propagation.jl")
+# include("models/external/solvers.jl")
+# include("models/external/externalmodel.jl")
+include("models/gp/standardization.jl")
+# include("models/model.jl")
+# include("models/polyharmonicspline.jl")
+# include("models/pce/pcebases.jl")
+# include("models/pce/polynomialchaosexpansion.jl")
+# include("models/responsesurface.jl")
+# include("models/imprecise/propagation.jl")
 
-include("modelupdating/bayesianupdating.jl")
-include("modelupdating/bayesianMAP.jl")
+# include("modelupdating/bayesianupdating.jl")
+# include("modelupdating/bayesianMAP.jl")
 
-include("reliability/form.jl")
-include("reliability/probabilityoffailure.jl")
-include("reliability/probabilityoffailure_imprecise.jl")
+# include("reliability/form.jl")
+# include("reliability/probabilityoffailure.jl")
+# include("reliability/probabilityoffailure_imprecise.jl")
 
-include("sensitivity/gradient.jl")
-include("sensitivity/sobolindices.jl")
+# include("sensitivity/gradient.jl")
+# include("sensitivity/sobolindices.jl")
 
-include("simulations/doe.jl")
-include("simulations/montecarlo.jl")
-include("simulations/subset.jl")
+# include("simulations/doe.jl")
+# include("simulations/montecarlo.jl")
+# include("simulations/subset.jl")
 
-include("util/fourier-transform.jl")
+# include("util/fourier-transform.jl")
 
-if Sys.islinux()
-    HPC = false
-    HPC_account = "HPC_account_1"
-    HPC_partition = "CPU_partition"
-    if "HPC" in ARGS
-        HPC = true
-        HPC_account = ARGS[2]
-        HPC_partition = ARGS[3]
-        @warn "Running a slurm test with HPC=ON, using account $HPC_account and partition $HPC_partition. Several (20) small 1-task calculations will be submitted to slurm for testing in different job array configuations."
-    end
+# if Sys.islinux()
+#     HPC = false
+#     HPC_account = "HPC_account_1"
+#     HPC_partition = "CPU_partition"
+#     if "HPC" in ARGS
+#         HPC = true
+#         HPC_account = ARGS[2]
+#         HPC_partition = ARGS[3]
+#         @warn "Running a slurm test with HPC=ON, using account $HPC_account and partition $HPC_partition. Several (20) small 1-task calculations will be submitted to slurm for testing in different job array configuations."
+#     end
 
-    if HPC == false && !occursin("test/test_utilities", ENV["PATH"])
-        @warn "For slurm test to pass on Linux, test_utilities/sbatch must be added to PATH"
-        @warn "sbatch command line tool may use the fake test_utilities/sbatch"
-    end
-    include("hpc/slurm.jl")
-end
+#     if HPC == false && !occursin("test/test_utilities", ENV["PATH"])
+#         @warn "For slurm test to pass on Linux, test_utilities/sbatch must be added to PATH"
+#         @warn "sbatch command line tool may use the fake test_utilities/sbatch"
+#     end
+#     include("hpc/slurm.jl")
+# end
