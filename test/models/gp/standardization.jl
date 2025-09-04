6196775148edaf7aa18dffce72e62a8e01e67263
Cr0gan <felixmett@googlemail.com>
2025-09-04 18:17:21 +0200

    Add unit tests for gp data standardization

--- test/models/gp/standardization.jl
+++ test/models/gp/standardization.jl
@@ -1,194 +1,186 @@
+function make_standardizer(
+    dataframe, 
+    input_names, 
+    output,
+    it,
+    ot
+)
+    UncertaintyQuantification.DataStandardizer(
+        dataframe, input_names, output, 
+        UncertaintyQuantification.InputTransform(it),
+        UncertaintyQuantification.OutputTransform(ot),
+    )
+end
+
 @testset "GaussianProcessDataStandardizer" begin
-    single_input = RandomVariable(Normal(-1, 0.5), :x1)
-    multi_input = RandomVariable.([Uniform(-2, 0), Normal(-1, 0.5), Uniform(0, 1)], [:x1, :x2, :x3])
+    transforms = [
+        UncertaintyQuantification.IdentityTransform(),
+        UncertaintyQuantification.ZScoreTransform(),
+        UncertaintyQuantification.UnitRangeTransform(),
+        UncertaintyQuantification.StandardNormalTransform()
+    ]
 
     N = 10
     output = :y
 
-    df_single_in = sample(single_input, N)
-    df_single_in[!, output] = rand(N)
+    single_input = RandomVariable(Normal(-1, 0.5), :x1)
+    df_single = sample(single_input, N)
+    df_single[!, output] = rand(N)
 
-    df_multi_in = sample(multi_input, N)
-    df_multi_in[!, output] = df_single_in[!, output]
+    multi_input = RandomVariable.([Uniform(-2, 0), Normal(-1, 0.5), Uniform(0, 1)], [:x1, :x2, :x3])
+    df_multi = sample(multi_input, N)
+    df_multi[!, output] = df_single[!, output]
+    
+    names_single = propertynames(df_single[:, Not(output)])
+    names_multi = propertynames(df_multi[:, Not(output)])
 
-    single_input_names = propertynames(df_single_in[:, Not(output)])
-    multi_input_names = propertynames(df_multi_in[:, Not(output)])
+    for transform in transforms
+        @testset "$(nameof(typeof(transform)))" begin
+            for (testname, df, inputs, names) in [
+                    ("single input", df_single, single_input, names_single),
+                    ("multi input",  df_multi,  multi_input, names_multi)
+                ]
+                @testset "$testname" begin
+                    if isa(transform, UncertaintyQuantification.StandardNormalTransform)
+                        @test_throws ArgumentError UncertaintyQuantification.DataStandardizer(
+                            df, inputs, output,
+                            UncertaintyQuantification.InputTransform(transform),
+                            UncertaintyQuantification.OutputTransform(transform)
+                        )
+                        # Test output shapes for identity output transform to check StandardNormalTransform for inputs
+                        dts = UncertaintyQuantification.DataStandardizer(
+                            df, inputs, output,
+                            UncertaintyQuantification.InputTransform(transform),
+                            UncertaintyQuantification.OutputTransform(
+                                UncertaintyQuantification.IdentityTransform()
+                                )
+                        )
+                        Xin = dts.fᵢ(df)
+                        if testname == "single input"
+                            # input gets transformed to a Vector
+                            @test isa(Xin, Vector)
+                        else
+                            # input gets transformed to RowVecs
+                            @test isa(Xin, RowVecs)
+                        end
+                    else
+                        dts = UncertaintyQuantification.DataStandardizer(
+                            df, inputs, output,
+                            UncertaintyQuantification.InputTransform(transform),
+                            UncertaintyQuantification.OutputTransform(transform),
+                        )
 
-    @testset "IdentityTransform" begin
-        # 1D input
-        dts_single = UncertaintyQuantification.DataStandardizer(
-            df_single_in, single_input_names, output, 
-            UncertaintyQuantification.InputTransform(
-                UncertaintyQuantification.IdentityTransform()
-                ), 
-            UncertaintyQuantification.OutputTransform(
-                UncertaintyQuantification.IdentityTransform()
-                )
-        )
-        single_in_transformed = dts_single.fᵢ(df_single_in)
-        
-        @test isa(single_in_transformed, Vector)
-        @test all(single_in_transformed .== df_single_in[:, only(single_input_names)])
+                        Xin = dts.fᵢ(df)
+                        Xout = dts.fₒ(df)
+                        Yout = dts.fₒ⁻¹(Xout)
+                        var_Yout = dts.var_fₒ⁻¹(Xout)
+                        if testname == "single input"
+                            # input gets transformed to a Vector
+                            @test isa(Xin, Vector)
+                            if isa(transform, UncertaintyQuantification.IdentityTransform)
+                                # Test input scaling
+                                @test all(Xin .== df[!, only(names)])
+                                # Test output scaling
+                                @test all(Yout .== Xout)
+                                # Test output inverse transform
+                                @test all(df[!, output] .== Yout)
+                                # Test output inverse transform for variance
+                                @test all(df[!, output] .== var_Yout)
 
-        # ND input
-        dts_multi = UncertaintyQuantification.DataStandardizer(
-            df_multi_in, multi_input_names, output, 
-            UncertaintyQuantification.InputTransform(
-                UncertaintyQuantification.IdentityTransform()
-                ), 
-            UncertaintyQuantification.OutputTransform(
-                UncertaintyQuantification.IdentityTransform()
-                )
-        )
-        multi_in_transformed = dts_multi.fᵢ(df_multi_in)
+                            elseif isa(transform, UncertaintyQuantification.ZScoreTransform)
+                                # Test input scaling
+                                μ = mean(df[!, only(names)])
+                                σ = std(df[!, only(names)]) 
+                                Min = (df[!, only(names)] .- μ) ./ σ
+                                @test all(Xin .≈ Min)
 
-        @test isa(multi_in_transformed, RowVecs)
+                                # Test output scaling
+                                μ = mean(df[!, output])
+                                σ = std(df[!, output]) 
+                                Mout = (df[!, output] .- μ) ./ σ                                    
+                                @test all(Mout .≈ Xout)
+                                # Test output inverse transform
+                                @test all(df[!, output] .≈ Yout)
+                                # Test output inverse transform for variance
+                                @test all(σ^2 * Xout .≈ var_Yout)
 
-        RowVecsMatrix = mapreduce(rv -> rv', vcat, multi_in_transformed)
-        @test all(RowVecsMatrix .== Matrix(df_multi_in[:, multi_input_names]))
+                            elseif isa(transform, UncertaintyQuantification.UnitRangeTransform)
+                                # Test input scaling
+                                tmin, tmax = extrema(df[!, only(names)])
+                                shift = tmin
+                                scale = 1 / (tmax - tmin)
+                                Min = (df[!, only(names)] .- shift) * scale
+                                @test all(Xin .≈ Min)
 
-        # Output
-        out_transformed = dts_single.fₒ(df_single_in)
+                                # Test output scaling
+                                tmin, tmax = extrema(df[!, output])
+                                shift = tmin
+                                scale = 1 / (tmax - tmin)
+                                Mout = (df[!, output] .- shift) * scale                                   
+                                @test all(Mout .≈ Xout)
+                                # Test output inverse transform
+                                @test all(df[!, output] .≈ Yout)
+                                # Test output inverse transform for variance
+                                @test all(scale^2 * Xout .≈ var_Yout)
 
-        # single input and multi input related output transforms should do the same thing
-        @test all(out_transformed .== dts_multi.fₒ(df_multi_in))
+                            end
+                        else
+                            # input gets transformed to RowVecs
+                            @test isa(Xin, RowVecs)
+                            if isa(transform, UncertaintyQuantification.IdentityTransform)
+                                # Test input scaling
+                                Min = mapreduce(rv -> rv', vcat, Xin)
+                                @test all(Min .== Matrix(df[!, names]))
+                                # Test output scaling
+                                @test all(Yout .== Xout)
+                                # Test output inverse transform
+                                @test all(df[!, output] .== Yout)
+                                # Test output inverse transform for variance
+                                @test all(df[!, output] .== var_Yout)
 
-        # Mean and latent function samples just get identity transformed
-        @test all(df_single_in[!, output] .== dts_single.fₒ⁻¹(out_transformed))
-        @test all(df_multi_in[!, output] .== dts_multi.fₒ⁻¹(out_transformed))
+                            elseif isa(transform, UncertaintyQuantification.ZScoreTransform)
+                                # Test input scaling
+                                Xin = mapreduce(rv -> rv', vcat, Xin)
+                                μ = mean(Matrix(df[!, names]), dims=1)
+                                σ = std(Matrix(df[!, names]), dims=1) 
+                                Min = (Matrix(df[!, names]) .- μ) ./ σ
+                                @test all(Xin .≈ Min)
 
-        # Variance also just get identity transformed
-        @test all(df_single_in[!, output] .== dts_single.var_fₒ⁻¹(out_transformed))
-        @test all(df_multi_in[!, output] .== dts_multi.var_fₒ⁻¹(out_transformed))
-    end
+                                # Test output scaling
+                                μ = mean(df[!, output])
+                                σ = std(df[!, output]) 
+                                Mout = (df[!, output] .- μ) ./ σ                                    
+                                @test all(Mout .≈ Xout)
+                                # Test output inverse transform
+                                @test all(df[!, output] .≈ Yout)
+                                # Test output inverse transform for variance
+                                @test all(σ^2 * Xout .≈ var_Yout)
 
-    @testset "ZScoreTransform" begin
-        # 1D input
-        dts_single = UncertaintyQuantification.DataStandardizer(
-            df_single_in, single_input_names, output, 
-            UncertaintyQuantification.InputTransform(
-                UncertaintyQuantification.ZScoreTransform()
-                ), 
-            UncertaintyQuantification.OutputTransform(
-                UncertaintyQuantification.ZScoreTransform()
-                )
-        )
-        single_in_transformed = dts_single.fᵢ(df_single_in)
-        
-        @test isa(single_in_transformed, Vector)
+                            elseif isa(transform, UncertaintyQuantification.UnitRangeTransform)
+                                # Test input scaling
+                                Xin = mapreduce(rv -> rv', vcat, Xin)
+                                extrs = extrema(Matrix(df[!, names]), dims=1)
+                                shift = map(t -> t[1], extrs[1, :])
+                                scale = map(t -> 1 / (t[2] - t[1]), extrs[1, :])
+                                Min = (Matrix(df[!, names]) .- shift') .* scale'
+                                @test all(Xin .≈ Min)
 
-        μ = mean(df_single_in[:, only(single_input_names)])
-        σ = std(df_single_in[:, only(single_input_names)])
-        manually_scaled = (df_single_in[:, only(single_input_names)] .- μ) ./ σ
-        @test all(single_in_transformed .≈ manually_scaled)
+                                # Test output scaling
+                                tmin, tmax = extrema(df[!, output])
+                                shift = tmin
+                                scale = 1 / (tmax - tmin)
+                                Mout = (df[!, output] .- shift) * scale                                   
+                                @test all(Mout .≈ Xout)
+                                # Test output inverse transform
+                                @test all(df[!, output] .≈ Yout)
+                                # Test output inverse transform for variance
+                                @test all(scale^2 * Xout .≈ var_Yout)
 
-        # ND input
-        dts_multi = UncertaintyQuantification.DataStandardizer(
-            df_multi_in, multi_input_names, output, 
-            UncertaintyQuantification.InputTransform(
-                UncertaintyQuantification.ZScoreTransform()
-                ), 
-            UncertaintyQuantification.OutputTransform(
-                UncertaintyQuantification.ZScoreTransform()
-                )
-        )
-        multi_in_transformed = dts_multi.fᵢ(df_multi_in)
-
-        @test isa(multi_in_transformed, RowVecs)
-
-        RowVecsMatrix = mapreduce(rv -> rv', vcat, multi_in_transformed)
-        μ = mean(Matrix(df_multi_in[:, multi_input_names]), dims=1)
-        σ = std(Matrix(df_multi_in[:, multi_input_names]), dims=1)
-
-        manually_scaled = (Matrix(df_multi_in[:, multi_input_names]) .- μ) ./ σ
-        @test all(RowVecsMatrix .≈ manually_scaled)
-
-        # Output
-        out_transformed = dts_single.fₒ(df_single_in)
-
-        # single input and multi input related output transforms should do the same thing
-        @test all(out_transformed .== dts_multi.fₒ(df_multi_in))
-
-        # Mean and latent function samples get rescaled and shifted
-        @test all(df_single_in[!, output] .≈ dts_single.fₒ⁻¹(out_transformed))
-        @test all(df_multi_in[!, output] .≈ dts_multi.fₒ⁻¹(out_transformed))
-
-        # Variance gets multiplied by squared standard deviation used in ZScoreTransform
-        # Note: This usually gets applied to the GP posterior variance, here
-        # we just scale out_transformed back to verify it does the right thing
-        σ = std(df_single_in[:, output])
-
-        @test all(out_transformed * σ^2 .≈ dts_single.var_fₒ⁻¹(out_transformed))
-        @test all(out_transformed * σ^2 .≈ dts_multi.var_fₒ⁻¹(out_transformed))
-    end
-
-    @testset "UnitRangeTransform" begin
-        # 1D input
-        dts_single = UncertaintyQuantification.DataStandardizer(
-            df_single_in, single_input_names, output, 
-            UncertaintyQuantification.InputTransform(
-                UncertaintyQuantification.UnitRangeTransform()
-                ), 
-            UncertaintyQuantification.OutputTransform(
-                UncertaintyQuantification.UnitRangeTransform()
-                )
-        )
-        single_in_transformed = dts_single.fᵢ(df_single_in)
-        
-        @test isa(single_in_transformed, Vector)
-
-        tmin, tmax = extrema(df_single_in[:, only(single_input_names)])
-        scale = 1 / (tmax - tmin)
-        manually_scaled = (df_single_in[:, only(single_input_names)] .- tmin) * scale
-        @test all(single_in_transformed .≈ manually_scaled)
-
-        # ND input
-        dts_multi = UncertaintyQuantification.DataStandardizer(
-            df_multi_in, multi_input_names, output, 
-            UncertaintyQuantification.InputTransform(
-                UncertaintyQuantification.UnitRangeTransform()
-                ), 
-            UncertaintyQuantification.OutputTransform(
-                UncertaintyQuantification.UnitRangeTransform()
-                )
-        )
-        multi_in_transformed = dts_multi.fᵢ(df_multi_in)
-
-        @test isa(multi_in_transformed, RowVecs)
-
-        RowVecsMatrix = mapreduce(rv -> rv', vcat, multi_in_transformed)
-        mins_maxs = extrema(df_single_in[:, only(single_input_names)], dims=1)
-        tmin = map(t -> t[1], mins_maxs[1, :])
-        scale = map(t -> 1 / (t[2] - t[1]), mins_maxs[1, :])
-
-        manually_scaled = (Matrix(df_multi_in[:, multi_input_names]) .- tmin) .* scale
-        print(RowVecsMatrix)
-        print(manually_scaled)
-        @test all(RowVecsMatrix .≈ manually_scaled)
-
-        # Output
-        out_transformed = dts_single.fₒ(df_single_in)
-
-        # single input and multi input related output transforms should do the same thing
-        @test all(out_transformed .== dts_multi.fₒ(df_multi_in))
-
-        # Mean and latent function samples get rescaled and shifted
-        @test all(df_single_in[!, output] .≈ dts_single.fₒ⁻¹(out_transformed))
-        @test all(df_multi_in[!, output] .≈ dts_multi.fₒ⁻¹(out_transformed))
-
-        # Variance gets multiplied by squared scale used in UnitRangeTransform
-        # Note: This usually gets applied to the GP posterior variance, here
-        # we just scale out_transformed back to verify it does the right thing
-        tmin, tmax = extrema(df_single_in[:, output])
-        scale = 1 / (tmax - tmin)
-
-        print(out_transformed * (1/scale)^2)
-        print(dts_single.var_fₒ⁻¹(out_transformed))
-        @test all(out_transformed * (1/scale)^2 .≈ dts_single.var_fₒ⁻¹(out_transformed))
-        @test all(out_transformed * (1/scale)^2 .≈ dts_multi.var_fₒ⁻¹(out_transformed))
-    end
-
-    @testset "StandardNormalTransform" begin
-        #: TODO
+                            end
+                        end
+                    end
+                end
+            end
+        end
     end
 end
